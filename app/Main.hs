72e64d7a81b585278c36cad8a652e3d6dd8598c1
Soorya Narayan <nayaran.ayroos@gmail.com>
2018-02-17 06:15:55 +0530

    Format code

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,15 @@
 module Main where
 
-import Node 
-import Control.Concurrent    (forkIO, threadDelay,newEmptyMVar,putMVar,takeMVar,readChan, writeChan, newChan)
-import System.Environment 
+import           Control.Concurrent (forkIO, newChan, newEmptyMVar, putMVar,
+                                     readChan, takeMVar, threadDelay, writeChan)
+import           Node
+import           System.Environment
 
 main :: IO ()
-main = do 
-        args <- getArgs 
-        done <- newEmptyMVar
-        -- Runs these functions on different threads 
-        forkIO $ runUDPServerForever (args !! 0) (args !! 1) >> putMVar done ()
-        threadDelay 100000 -- wait one second
-        takeMVar done
-       
+main = do
+  args <- getArgs
+  done <- newEmptyMVar
+        -- Runs these functions on different threads
+  forkIO $ runUDPServerForever (args !! 0) (args !! 1) >> putMVar done ()
+  threadDelay 100000 -- wait one second
+  takeMVar done
