20708e643e743e2f642064a4bbfe48f114662e56
Soorya Narayan <nayaran.ayroos@gmail.com>
2018-02-17 06:33:16 +0530

    Add functionality to read from config file
    Addresses #9

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Control.Concurrent (forkIO, newChan, newEmptyMVar, putMVar,
                                      readChan, takeMVar, threadDelay, writeChan)
+import           Data.List          (find)
+import           Data.Maybe         (fromMaybe)
 import           Node
 import           System.Environment
+
+import qualified Data.Configurator  as C
+import           Data.Text          hiding (find)
+
+data Config = Config
+  { ipAddress :: String
+  , portNo    :: String
+  } deriving (Eq, Show)
+
+readConfig :: FilePath -> IO Config
+readConfig cfgFile = do
+  cfg          <- C.load [C.Required cfgFile]
+  ipAddress    <- C.require cfg "node.ipAddress"
+  portNo       <- C.require cfg "node.portNo"
+  return $ Config ipAddress portNo
+
 
 main :: IO ()
 main = do
   args <- getArgs
+  let cfgFilePath =
+        fromMaybe (error "Usage: ./kademlia-exe --config /file/path") $
+        find (/= "--config") args
+  cfg <- readConfig cfgFilePath
   done <- newEmptyMVar
         -- Runs these functions on different threads
-  forkIO $ runUDPServerForever (args !! 0) (args !! 1) >> putMVar done ()
+  forkIO $ runUDPServerForever (ipAddress cfg) (portNo cfg) >> putMVar done ()
   threadDelay 100000 -- wait one second
   takeMVar done
