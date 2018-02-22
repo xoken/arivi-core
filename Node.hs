5030697200ca1116e3857715cac96716c2f11181
Ankit Singh <ankit.sachyantra@gmail.com>
2018-02-22 17:10:34 +0530

    Added K-buckets using TChans with basics features. Node can now add a node making request to its k-bucket but they need to be refined one address generaion and distance calculation functions are thre

--- Node.hs
+++ Node.hs
@@ -1,23 +1,33 @@
 module Node
-  ( runUDPServerForever
+  ( runUDPServerForever,
+    messageHandler,
+    loadDefaultPeers,
+    networkClient,
+    addToKbChan,
+    convertToSockAddr,
+    generateNodeId
   ) where
 
 import           Control.Concurrent        (forkIO, newChan, newEmptyMVar,
                                             putMVar, readChan, takeMVar,
-                                            threadDelay, writeChan)
+                                            threadDelay, writeChan,isEmptyChan)
 import           Control.Monad             (forever)
 import qualified Data.ByteString.Char8     as C
 import           Network.Socket            hiding (recv)
 -- import qualified Network.Socket.ByteString (recv, sendAll)
 import qualified Network.Socket.ByteString as N (recv, recvFrom, sendAll,
                                                  sendAllTo, sendTo)
+import qualified Network.Socket.Internal   as M 
 --import Data.List.Split
 import           Control.Concurrent.Async
 import           System.Environment
 import           System.Random             (randomRIO)
-
-workerCount = 5
-
+import qualified Data.Map.Strict           as Map
+import           Data.Maybe 
+import           System.Random
+import qualified Data.List.Split    as S  
+import           Data.Word 
+      
 -- Helper functions to extract value from 3-tuple
 extractFirst :: (a, b, c) -> a
 extractFirst (a,_,_) = a
@@ -28,65 +38,100 @@ extractSecond (_,b,_) = b
 extractThird :: (a, b, c) -> c
 extractThird (_,_,c) = c
 
-messageHandler inboundChan outboundChan workerId = forkIO $ forever $ do
+data Message = PING
+               |PONG
+               |FIND_NODE { 
+                   nodeId     :: [Char]
+                ,  find       :: [Char]
+                ,  address    :: [Char] 
+                ,  seqNo      :: [Char]
+                ,  signature  :: [Char] 
+                }
+               |FN_RESP {
+                    nodeId    :: [Char]
+                ,   knodes    :: [(String,String)] 
+                ,   address   :: [Char] 
+                ,   seqNo     :: [Char]
+                ,   signature :: [Char] 
+                }            
+               deriving (Show)
+
+
+messageHandler inboundChan outboundChan peerChan workerId = forkIO $ forever $ do
     msg <- readChan inboundChan
+    --writeChan outboundChan ("PONG",addr,sock)
     case msg of
         ("PING",addr,sock) -> writeChan outboundChan ("PONG",addr,sock)
-        ("FIND_NODE",addr,sock) -> writeChan outboundChan ("Uh Ah..",addr,sock)
+        
+        ("FIND_NODE",addr,sock) -> do 
+            -- add the asking node to corresponding k-bucket 
+            -- Just for testing 
+            rnd <- randomRIO (1::Int,10::Int)
+            writeChan peerChan (extractSecond msg,rnd)
+            -- clientResponse <- findKNodes sock 
+            writeChan outboundChan ("FN_RESP",addr,sock)
+        
+        ("FN_RESP",addr,sock) -> do 
+            print "Response recieved"
+            -- We have to issue subsequent FIND_NODE to each node recieved here 
 
-networkClient outboundChan workerId = forkIO $ forever $ do
+networkClient outboundChan workerId = forkIO $ forever $ do 
     msg <- readChan outboundChan
-    print $ show (extractThird msg)
-    -- responding back to remote node using local UDP listen port
-    do N.sendTo (extractThird msg) (C.pack $ extractFirst msg) (extractSecond msg)
+    N.sendTo (extractThird msg) (C.pack $ extractFirst msg) (extractSecond msg)          
 
+addToKbChan kbChan peerChan workerId = forkIO $ forever $ do
+    msg <- readChan peerChan 
+    rl <- isEmptyChan kbChan 
+    case rl of 
+        True -> do 
+            let temp = Map.empty 
+                temp2 = Map.insert (snd msg) (fst msg : []) temp  
+            writeChan kbChan temp2
+            print temp2
+        False -> do 
+                kb  <- readChan kbChan
+                if (Map.lookup (snd msg) kb == Nothing)
+                    then do
+                        let temp = Map.insert (snd msg) (fst msg:[]) kb 
+                        writeChan kbChan temp 
+                        print temp
+                    else do 
+                        let temp    = Map.lookup (snd msg) kb 
+                            temp2   = fromMaybe ((fst msg):[]) temp 
+                            temp3   = (fst msg) : temp2
+                            payLoad = Map.insert (snd msg) (temp3) kb
+                        print payLoad     
+                        writeChan kbChan payLoad
+                                                 
 -- This create a UDP server which is constantly listenting for requests
-runUDPServerForever :: String -> String -> IO ()
-runUDPServerForever local_ip local_port  = do
+-- runUDPServerForever :: String -> String -> IO (Chan) -> IO ()
+runUDPServerForever local_ip local_port inboundChan servChan = do
     addrinfos <- getAddrInfo Nothing (Just local_ip) (Just local_port)
     let serveraddr = head addrinfos
     sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
     bind sock (addrAddress serveraddr)
+
+    writeChan servChan sock 
+
     print "Server now listening for requests"
-
-    inboundChan <- newChan
-    outboundChan <- newChan
-
-    mapM_ (messageHandler inboundChan outboundChan) [1..workerCount]
-    mapM_ (networkClient outboundChan) [1..workerCount]
-
     forever $
          do
             (mesg, socaddr2) <- N.recvFrom sock 4096
             print (mesg,socaddr2)
             writeChan inboundChan ((C.unpack mesg),socaddr2,sock)
 
--- Create a connection to the server and returns socket
-createSocket server_ip server_port = do
-    addrinfos <- getAddrInfo Nothing (Just server_ip) (Just server_port)
-    let serveraddr = head addrinfos
-    sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
-    connect sock (addrAddress serveraddr)
-    return (sock)
+--covnert [String] to [SockAddr]  
+convertToSockAddr :: [Char] -> SockAddr 
+convertToSockAddr x  = fSockAddr
+    where addrString = S.splitOn ":" x
+          remotePort = read $ addrString !! 1 :: M.PortNumber 
+          temp       = S.splitOn "." (addrString !! 0)  
+          temp2      = case (Prelude.map (read :: String -> Word8) temp) of [a,b,c,d] -> (a,b,c,d)
+          remoteIp   = tupleToHostAddress temp2
+          fSockAddr  = SockAddrInet remotePort remoteIp
 
- -- Connects to server and sends it a message
-sendMessage :: String -> Socket -> IO ()
-sendMessage s sock = do
-    N.sendAll sock $ C.pack s
-    (mesg, socaddr2) <- N.recvFrom sock 4096
-    print (mesg,socaddr2)
-
--- This calls sendMessage to send multiple messages to server
-run 0 _  = return ()
-run n client_soc = do
-    sendMessage "PING" client_soc
-    run (n-1) client_soc
-
-main :: IO ()
-main = do
-        args <- getArgs
-        done <- newEmptyMVar
-        -- Runs these functions on different threads
-        forkIO $ runUDPServerForever (args !! 0) (args !! 1) >> putMVar done ()
-        threadDelay 100000 -- wait one second
-        takeMVar done
+loadDefaultPeers peerList outboundChan peerChan servChan = do 
+    sock <- readChan servChan 
+    mapM_ (writeChan peerChan) (zip peerList (replicate (length peerList) 1))
+    mapM_ (writeChan outboundChan) (zip3 (repl "FIND_NODE") peerList (repl (sock)))
+    where repl = replicate (length peerList) 
