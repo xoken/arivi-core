9d04fe74b2c5a99dccbb297b12cae2f2fe3baf01
Mahesh Uligade <maheshuligade@gmail.com>
2018-02-22 17:11:40 +0530

    Added XorDistance module and it's tests
    Can be used by importing Utils.XorDistance module
    
    (cherry picked from commit 463bd180da842a0c48ec63687cc63593b3ceee2b)
    (cherry picked from commit cc32710da472587b484b8bca4524d5414f355bc0)

---
+++ XorDistance.hs
@@ -0,0 +1,31 @@
+module Utils.XorDistance(
+
+    getXorDistance,
+    getXorDistancelog2,
+)
+where 
+
+
+import Data.Bits
+import Numeric (showHex,showIntAtBase)
+import Data.Char (intToDigit,digitToInt)
+import Data.ByteString.Char8 (ByteString)
+
+
+
+fn str =  (digitToInt str ):: Int
+hexToDigits str = map fn str
+
+hexToDec [] _ _  = 0; 
+hexToDec (xs:x) index length = hexToDec(x) (index+1) length + (16 ^ index)*xs
+
+
+hexToDecimal lst = hexToDec (reverse lst) 0 (length lst)
+
+
+
+biWisexorOfKeys firstNodeId secondNodeId = zipWith xor (hexToDigits firstNodeId) (hexToDigits secondNodeId) 
+
+getXorDistance firstNodeId secondNodeId = hexToDecimal(map toInteger (biWisexorOfKeys firstNodeId secondNodeId))
+
+getXorDistancelog2 number = logBase 2 (fromIntegral number)
