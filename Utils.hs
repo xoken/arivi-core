cd3154885ab8f5c3978108eeb2b113ccacba0d20
Mahesh Uligade <maheshuligade@gmail.com>
2018-03-01 16:26:52 +0530

    Added toHex function to convert Public Key to Hex format
    (cherry picked from commit d85c6e358cfcf6e3be12af35e728fe5d8ec38219)

--- Utils.hs
+++ Utils.hs
@@ -10,11 +10,25 @@ module Crypto.Utils
 import Crypto.PubKey.Ed25519 (SecretKey,PublicKey,secretKey,toPublic,sign,verify)
 import Crypto.Error (throwCryptoError)
 import Data.ByteString.Char8 (ByteString)
+import Data.ByteArray (convert)
+import Data.ByteString.Base16 (encode)
 
 
+-- | Takes a 32 bytes seed and produces SecretKey
 getSecretKey :: ByteString -> SecretKey
 getSecretKey seedString = throwCryptoError (secretKey seedString)
 
 
+-- | Generatees Public Key using the given Secret Key
 getPublicKey :: SecretKey -> PublicKey
 getPublicKey secretKey = toPublic secretKey
+
+
+-- | Takes PublicKey as input and extracts the string part of PublicKey
+toByteString :: PublicKey -> ByteString
+toByteString mPublicKey = ((Data.ByteArray.convert mPublicKey) :: ByteString)
+
+
+-- | Converts PublicKey format to Hexadecimal format
+toHex :: PublicKey -> ByteString
+toHex mPublicKey = (Data.ByteString.Base16.encode (toByteString mPublicKey))
