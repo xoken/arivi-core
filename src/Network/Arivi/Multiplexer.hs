dee5438b7a71b47513f9745a1239ce4be150f888
Ankit Singh <ankitsiam@gmail.com>
2018-03-26 18:07:18 +0530

    Created files for module required

---
+++ src/Network/Arivi/Multiplexer.hs
@@ -0,0 +1,17 @@
+module Network.Arivi.Multiplexer
+(
+
+) where 
+    
+import qualified Data.Map.Strict                    as Map 
+
+
+
+-- ariviConfig   = AriviConifg x y z 
+-- ariviInstance = getAriviInstance ariviConfig 
+
+-- register ariviInstance (0,messageHandler)
+-- register ariviInstance (1,messageHandler)
+-- register ariviInstance (1,messageHandler)
+
+-- runAriviInstance ariviInstance 
---
+++ src/Network/Arivi/NetworkClient.hs
@@ -0,0 +1,32 @@
+module Network.Arivi.networkClient 
+(
+
+) where 
+
+import           Control.Concurrent                         (forkIO,ThreadId,newEmptyMVar,putMVar,
+                                                            takeMVar)
+import           Control.Concurrent.MVar
+import           Control.Monad                              (forever)
+import           Network.Socket                              
+import           Control.Concurrent.STM                     (atomically,TChan,TMVar,newTMVar,
+                                                            newTChan,writeTChan,readTChan,
+                                                            readTMVar)
+import qualified Network.Socket.ByteString          as N    (recvFrom,sendTo) 
+import qualified Data.ByteString.Char8              as C 
+import qualified Data.List.Split                    as S 
+import           Data.Word 
+import           Data.Maybe                                  (fromMaybe)
+import qualified Data.Map.Strict                    as Map 
+
+
+networkClient :: TChan (C.ByteString,SockAddr)
+              -> TMVar (SockAddr,Socket)
+              -> Int 
+              -> IO ThreadId
+
+networkClient outboundChan socketTMVar workerId = forkIO $ forever $ do 
+    sockMsg <- atomically $ readTMVar socketTMVar 
+    msg     <- atomically $ readTChan outboundChan
+    let pl = fst msg  
+
+    N.sendTo (snd sockMsg) (pl) (fst sockMsg)
