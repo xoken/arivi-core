8fd4e2996f3e9c66066a7f626da24e845f2b1834
Ankit Singh <ankitsiam@gmail.com>
2018-03-27 17:15:54 +0530

    Finalized fundamental types for arivi

--- src/Network/Arivi/Types.hs
+++ src/Network/Arivi/Types.hs
@@ -1,6 +1,6 @@
 module Network.Arivi.Types 
 (
-Payload     (..),
+PayLoad     (..),
 Frame       (..),
 Socket,
 SockAddr 
@@ -12,36 +12,51 @@ import qualified Data.Map.Strict                    as Map
 import           Data.UUID                                  (UUID)
 import           Data.Int                                   (Int16,Int64)   
 
--- | Structure to hold the Payload which arivi will send and receive (PENDING)
 
-type ConnectionId   = Int64  
-type PayloadLength  = Int16    
+type SessionId      = Int64  
+type PayLoadLength  = Int16    
 type FragmentNumber = Integer 
 type MessageId      = UUID 
 type SubProtocol    = Int 
 
 data Frame   = Frame {
-                    version       :: Version 
-                ,   payLoadMarker :: PayloadMarker 
-                ,   opcode        :: Opcode 
-                ,   publicFlags   :: PublicFlags     
-                ,   payload       :: Payload 
-                ,   connectionId  :: ConnectionId   
-                ,   payloadLength :: PayloadLength 
-                ,   transport     :: Transport
-                ,   encoding      :: Encoding 
-            } deriving (Show)
+                    version        :: Version 
+                ,   publicFlags    :: PublicFlags
+                ,   encoding       :: Encoding  
+                ,   transport      :: Transport
+                ,   opcode         :: Opcode 
+                ,   fragmentNumber :: FragmentNumber
+                ,   payLoadLength  :: PayLoadLength     
+                ,   payLoadMarker  :: PayLoadMarker 
+                ,   payLoad        :: PayLoad 
+               } deriving (Show)
 
 data Version
     = V0 
     | V1 
     deriving (Eq, Ord, Show)
 
-data PayloadMarker = PayloadMarker {
-                    subProtocol    :: SubProtocol 
-                ,   fragmentNumber :: FragmentNumber
-                ,   messageId      :: MessageId
-            } deriving (Show)
+data PublicFlags  = PublicFlags {
+                    finalFragment :: Bool 
+                ,   initiator     :: Bool 
+                ,   ecncryption   :: EncryptionType 
+            } deriving (Show) 
+
+data EncryptionType = NONE 
+                      | AES256_CTR
+                      | CHACHA_POLY 
+                      deriving (Show)
+
+data Encoding = UTF_8
+                | ASCII 
+                | CBOR 
+                | JSON 
+                | PROTO_BUFF 
+                deriving (Show)
+
+data Transport = UDP 
+                 | TCP 
+                 deriving (Show)
 
 data Opcode       = ERROR 
                     | HANDSHAKE_REQUEST 
@@ -52,32 +67,13 @@ data Opcode       = ERROR
                     | PING  
                     | PONG 
                     deriving (Show)
-
-data PublicFlags  = PublicFlags {
-                    finalFragment :: Bool 
-                ,   textOrBinary  :: Bool 
-                ,   initiator     :: Bool 
-                ,   ecncryption   :: EncryptionType 
-            } deriving (Show)
-
-data EncryptionType = NONE 
-                      | AES_CTR
-                      | CHA_CHA_POLY 
-                      deriving (Show)
-                      
-data Payload = Payload C.ByteString 
-               deriving (Show)
-
-data Transport = UDP 
-                 | TCP 
-                 deriving (Show)
-                 
-data Encoding = UTF_8
-                | ASCII 
-                | CBOR 
-                | JSON 
-                | PROTO_BUFF 
-                deriving (Show)
                
 
-                    
+data PayLoadMarker = PayLoadMarker {
+                    subProtocol    :: SubProtocol 
+                ,   messageId      :: MessageId
+            } deriving (Show)
+                      
+data PayLoad = PayLoad C.ByteString
+               deriving (Show)
+                
