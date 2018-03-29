53072b6a6c6656d41578b6fdf293ea5dd00c6514
Ankit Singh <ankitsiam@gmail.com>
2018-03-29 17:15:48 +0530

    Added Stream to arivi Instance

--- src/Network/Arivi/Stream.hs
+++ src/Network/Arivi/Stream.hs
@@ -1,32 +1,32 @@
-module Network.Arivi.Stream 
+module Network.Arivi.Stream
 (
     runTCPServerForever
-) where 
+) where
 
 import           Control.Concurrent                         (forkIO,ThreadId,newEmptyMVar,putMVar,
                                                             takeMVar)
 import           Control.Concurrent.MVar
 import           Control.Monad                              (forever)
-import           Network.Socket                              
+import           Network.Socket
 import           Control.Concurrent.STM                     (atomically,TChan,TMVar,newTMVar,
                                                             newTChan,writeTChan,readTChan,
                                                             readTMVar)
-import qualified Network.Socket.ByteString          as N    (recvFrom,sendTo) 
-import qualified Data.ByteString.Char8              as C 
-import qualified Data.List.Split                    as S 
-import           Data.Word 
+import qualified Network.Socket.ByteString          as N    (recvFrom,sendTo)
+import qualified Data.ByteString.Char8              as C
+import qualified Data.List.Split                    as S
+import           Data.Word
 import           Data.Maybe                                  (fromMaybe)
 import qualified Network.Arivi.Multiplexer          as MP    (Registry)
 
 
-runTCPServerForever :: SockAddr
-                    -> Socket  
-                    -> MP.Registry 
-                    -> IO ThreadId 
+runTCPServerForever :: Socket
+                    -> SockAddr
+                    -> MP.Registry
+                    -> IO ()
 
-runTCPServerForever sockAddr sock messageHandler = do
-    bind sock sockAddr 
-    forkIO $ forever $
+runTCPServerForever sock sockAddr messageHandler = do
+    bind sock sockAddr
+    forever $
          do
             (mesg, socaddr2) <- N.recvFrom sock 4096
             print ""
