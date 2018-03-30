f8dc6e94a5481b289248c308fc66bfd907493f1e
Ankit Singh <ankitsiam@gmail.com>
2018-03-30 18:23:17 +0530

    Fixed linting errors & removed UDP server

--- Kademlia/XorDistance.hs
+++ Kademlia/XorDistance.hs
@@ -13,8 +13,13 @@ import           Numeric               (showHex, showIntAtBase)
 
 
 
+<<<<<<< HEAD
+fn str      =  digitToInt str :: Int
+hexToDigits = map fn
+=======
 fn str =  digitToInt str :: Int
 hexToDigits  = map fn
+>>>>>>> 8926b49a345627bedf9f05a30f218477f1271cfb
 
 hexToDec [] _ _              = 0;
 hexToDec (xs:x) index length = hexToDec x (index+1) length + (16 ^ index)*xs
@@ -24,9 +29,16 @@ hexToDecimal lst = hexToDec (reverse lst) 0 (length lst)
 
 
 
+<<<<<<< HEAD
+biWisexorOfKeys firstNodeId secondNodeId = zipWith xor (hexToDigits firstNodeId)
+                                            (hexToDigits secondNodeId)
+=======
 biWisexorOfKeys firstNodeId secondNodeId = zipWith xor (hexToDigits firstNodeId) (hexToDigits secondNodeId)
+>>>>>>> 8926b49a345627bedf9f05a30f218477f1271cfb
 
-getRawXor firstNodeId secondNodeId = hexToDecimal(map toInteger (biWisexorOfKeys firstNodeId secondNodeId))
+getRawXor firstNodeId secondNodeId = hexToDecimal(map toInteger
+                                            (biWisexorOfKeys firstNodeId
+                                            secondNodeId))
 
 -- getXorDistancelog2 number = logBase 2 (fromIntegral number)
 
@@ -35,4 +47,9 @@ getRawXor firstNodeId secondNodeId = hexToDecimal(map toInteger (biWisexorOfKeys
 getXorDistance firstNodeId secondNodeId = if rawXor == 0
                                             then 0
                                           else logBase 2 (fromIntegral rawXor)
+<<<<<<< HEAD
+                                          where rawXor = getRawXor firstNodeId
+                                                    secondNodeId
+=======
                                           where rawXor = getRawXor firstNodeId secondNodeId
+>>>>>>> 8926b49a345627bedf9f05a30f218477f1271cfb
