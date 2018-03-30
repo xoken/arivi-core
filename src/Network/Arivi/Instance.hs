30bd027f9257c099fd9eb8466b4d926f125f1322
Ankit Singh <ankitsiam@gmail.com>
2018-03-30 18:10:59 +0530

    added comments for Instance.hs

--- src/Network/Arivi/Instance.hs
+++ src/Network/Arivi/Instance.hs
@@ -19,17 +19,17 @@ import           Network.Arivi.Types
 import           Network.Socket
 
 
--- | Strcuture to hold the arivi configurations can also contain more parameters but for now
---   just contain 3
+-- | Strcuture to hold the arivi configurations can also contain more
+--   parameters but for now just contain 3
 data AriviConfig    = AriviConfig {
                         hostip  :: String
                     ,   udpport :: String
                     ,   tcpPort :: String
                     } deriving (Show)
 
--- | Strcuture which holds all the information about a running arivi Instance and can be passed
---   around to different functions to differentiate betweeen different instances of
---   arivi.
+-- | Strcuture which holds all the information about a running arivi Instance
+--   and can be passed around to different functions to differentiate betweeen
+--   different instances of arivi.
 data AriviHandle    = AriviHandle {
                         ariviUDPSock :: (Socket,SockAddr)
                     ,   ariviTCPSock :: (Socket,SockAddr)
@@ -54,8 +54,8 @@ getAriviInstance ac = do
     tcpt <- newEmptyMVar
     return (AriviHandle ariviUdpSock arivitcpSock udpt tcpt registry)
 
--- | Starts an arivi instance from ariviHandle which contains all the information required to run
---   an arivi instance.
+-- | Starts an arivi instance from ariviHandle which contains all the
+--   information required to run an arivi instance.
 runAriviInstance :: AriviHandle
                  -> IO ()
 
@@ -71,9 +71,9 @@ runAriviInstance ah = do
     wait tid1
     wait tid2
 
--- | Register callback functions for subprotocols which will be fired when arivi recieves a
---   message meant for a particular subprotocl essentially passing the control to subprotocol
---   with the message.
+-- | Register callback functions for subprotocols which will be fired when
+--   arivi recieves a message meant for a particular subprotocl essentially
+--   passing the control to subprotocol with the message.
 register :: AriviHandle
             -> Int               -- Subprotocol Code
             -> (PayLoad -> IO()) -- Subprotocol Message Handler
@@ -85,8 +85,8 @@ register :: AriviHandle
 
 register ah key value protocol transport encryptionType encoding = undefined
 
--- | assigns a unique session for each connection & subprotocol between two nodes which is
---   identified by a sessionID
+-- | assigns a unique session for each connection & subprotocol between two
+--   nodes which is identified by a sessionID
 createSession :: PortNumber
               -> HostAddress
               -> ContextID
