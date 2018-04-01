c15846418074b22ddc043a730e66d742cc881132
Mahesh Uligade <maheshuligade@gmail.com>
2018-04-01 19:37:06 +0530

    Fixed hlint warnings for Crypto.

--- src/Network/Arivi/Crypto/Utils/Keys/Signature.hs
+++ src/Network/Arivi/Crypto/Utils/Keys/Signature.hs
@@ -1,8 +1,12 @@
 -- |
--- Module : Crypto.Utils.Keys.Signature
+-- Module      : Crypto.Utils.Keys.Signature
+-- License     : 
+-- Maintainer  : Mahesh Uligade <maheshsuligade@gmail.com>
+-- Stability   :
+-- Portability 
 -- 
 -- This module is made for verifying messages between two parties
-
+-- 
 
 module Crypto.Utils.Keys.Signature
 (
@@ -10,17 +14,17 @@ module Crypto.Utils.Keys.Signature
     getPublicKey,
     sign,
     verify,
-<<<<<<< HEAD
-    generateKeyPair
-=======
     generateKeyPair,
-    toHex,
-    hexToPublicKey 
->>>>>>> 4ad2acf... Added  function
+    publicKeytoHex,
+    hexToPublicKey,
+    secretKeyToHex,
+    hexToSecretKey,
+    PublicKey,
+    SecretKey
 ) where
 
 
-import Crypto.PubKey.Ed25519 (SecretKey,PublicKey,secretKey,toPublic,sign,verify,publicKey)
+import Crypto.PubKey.Ed25519 (SecretKey,PublicKey,secretKey,toPublic,sign,verify,publicKey,secretKey)
 import Crypto.Error (throwCryptoError)
 import Data.ByteString.Char8 (ByteString)
 import Data.ByteArray (convert)
@@ -34,25 +38,38 @@ getSecretKey seedString = throwCryptoError (secretKey seedString)
 
 -- | Generates Public Key using the given Secret Key
 getPublicKey :: SecretKey -> PublicKey
-getPublicKey secretKey = toPublic secretKey
+getPublicKey = toPublic 
 
 
 -- | Takes PublicKey as input and extracts the string part of PublicKey
 toByteString :: PublicKey -> ByteString
-toByteString mPublicKey = ((Data.ByteArray.convert mPublicKey) :: ByteString)
+toByteString mPublicKey = Data.ByteArray.convert mPublicKey :: ByteString
 
 
 -- | Converts PublicKey format to Hexadecimal format
-toHex :: PublicKey -> ByteString
-toHex mPublicKey = (Data.ByteString.Base16.encode (toByteString mPublicKey))
+publicKeytoHex :: PublicKey -> ByteString
+publicKeytoHex mPublicKey = Data.ByteString.Base16.encode (toByteString mPublicKey)
 
 -- | Converts PublicKey from hex form to PublicKey form
 hexToPublicKey :: ByteString -> PublicKey
-hexToPublicKey hexPublicKey = (Crypto.Error.throwCryptoError (Crypto.PubKey.Ed25519.publicKey (fst (Data.ByteString.Base16.decode hexPublicKey))))
+hexToPublicKey hexPublicKey = Crypto.Error.throwCryptoError (Crypto.PubKey.Ed25519.publicKey (fst (Data.ByteString.Base16.decode hexPublicKey)))
 
+
+-- | This function generates (SecretKey,PublicKey) pair using Raaz's Random Seed
+-- generation 
 generateKeyPair :: IO (SecretKey, PublicKey)
 generateKeyPair = do 
-                 randomSeed <- (Crypto.Utils.Random.getRandomByteString 32)
+                 randomSeed <- Crypto.Utils.Random.getRandomByteString 32
                  let secretKey = getSecretKey randomSeed
                  let publicKey = getPublicKey secretKey
                  return (secretKey,publicKey)
+
+
+-- | This function is used for converting Secret Key from SecretKey form to hex form
+secretKeyToHex :: SecretKey-> ByteString
+secretKeyToHex mSecretKey= Data.ByteString.Base16.encode (Data.ByteArray.convert mSecretKey ::ByteString)
+
+
+-- | This function is used for converting Secret Key from hex form to SecretKey form
+hexToSecretKey :: ByteString -> SecretKey
+hexToSecretKey hexSecretKey = Crypto.Error.throwCryptoError (Crypto.PubKey.Ed25519.secretKey (fst (Data.ByteString.Base16.decode hexSecretKey)))
