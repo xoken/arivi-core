9c39cf22c55361a106f1cd2464656b5e3991777b
Mahesh Uligade <maheshuligade@gmail.com>
2018-04-01 19:39:02 +0530

    Fixed Haddock errors in Crypto.

--- src/Network/Arivi/Crypto/Utils/Random.hs
+++ src/Network/Arivi/Crypto/Utils/Random.hs
@@ -1,3 +1,23 @@
+-- |
+-- Module      : Crypto.Utils.Random
+-- License     : 
+-- Maintainer  : Mahesh Uligade <maheshsuligade@gmail.com>
+-- Stability   :
+-- Portability :
+-- 
+-- This module provides random generation using Raaz's random no generation 
+-- with `securely`
+-- 
+-- securely : 
+-- Run a memory action with the internal memory allocated from a 
+-- locked memory buffer. This memory buffer will never be swapped out by the 
+-- operating system and will be wiped clean before releasing.
+-- Memory locking is an expensive operation and usually there would be a limit to 
+-- how much locked memory can be allocated. Nonetheless, actions that work with 
+-- sensitive information like passwords should use this to run an memory action.[1]
+-- 1.<https://hackage.haskell.org/package/raaz-0.2.0/docs/Raaz-Core-Memory.html#v:securely>
+-- 
+
 module Crypto.Utils.Random
 (
     getRandomByteString
