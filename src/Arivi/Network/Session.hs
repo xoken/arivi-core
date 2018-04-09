25c6cf76dc658cb39184ef817f06f2543e67d277
Mahesh Uligade <maheshuligade@gmail.com>
2018-04-09 11:18:39 +0530

    Updated SessionId genearation method
    SessionId will be concatenation of sericeCode and connectionId

--- src/Arivi/Network/Session.hs
+++ src/Arivi/Network/Session.hs
@@ -12,29 +12,29 @@
 module Arivi.Network.Session
 (
     SessionId,
+    ServiceCode,
     Session (..),
-    getUniqueSessionId,
-    genSessionId,
     createSession,
     closeSession
 ) where
 
 
-import           Data.ByteString.Base16             (encode)
-import           Data.ByteString.Char8              (ByteString, pack)
-import           Data.HashMap.Strict                (HashMap, delete, empty,
-                                                     insert, member)
+import           Data.ByteString.Char8    (ByteString, append, pack)
+import           Data.HashMap.Strict      (HashMap, delete, empty, insert,
+                                           member)
 
-import           Arivi.Crypto.Utils.Keys.Encryption as Encryption
-import           Arivi.Crypto.Utils.Random          (getRandomByteString)
-import           Arivi.Kademlia.Types               (HostAddress, NodeId)
 import           Arivi.Network.Connection
-import           Arivi.Network.Types                (EncodingType,
-                                                     ServiceContext,
-                                                     TransportType)
+import           Arivi.Network.Types      (EncodingType, ServiceContext,
+                                           TransportType)
 
 -- | SessionId is type synonym for ByteString
 type SessionId = ByteString
+
+-- | ServiceCode is type synonym for ByteString
+type ServiceCode = ByteString
+
+
+
 
 -- | (sessionId,Session) are (key,value) pair in HashMap that stores
 -- information about all the session uniquely
@@ -47,48 +47,24 @@ data Session = Session {
 
 
 
-
-
-
--- | Generates a random 4 Byte SessionId using Raaz's random ByteString
--- generation
-genSessionId :: IO SessionId
-genSessionId = getRandomByteString 4 >>=
-                                    \byteString -> return (encode byteString)
-
-
--- | Generates unique SessionId by checking it is already present in given
--- HashMap
-getUniqueSessionId :: HashMap ByteString Session -> IO SessionId
-getUniqueSessionId hashmap = do
-                                sessionId <- genSessionId
-
-                                if member sessionId hashmap
-                                    then  getUniqueSessionId hashmap
-                                    else
-                                        return sessionId
-
-
-
-
 -- | Creates Unique session and stores in given HashMap
 
-createSession :: ServiceContext
-             -> ConnectionId
-             -> EncodingType
-             -> HashMap SessionId Session
-             -> IO (SessionId,HashMap SessionId Session)
-createSession serviceContext connectionId
+createSession :: ServiceCode
+              -> ServiceContext
+              -> ConnectionId
+              -> EncodingType
+              -> HashMap SessionId Session
+              -> (SessionId,HashMap SessionId Session)
+createSession serviceCode serviceContext connectionId
               encodingType sessionHashmap =
-
-                getUniqueSessionId sessionHashmap
-                    >>= \uniqueSessionId
-                    -> return
-                   (uniqueSessionId,
-                    Data.HashMap.Strict.insert uniqueSessionId
-                                 (Session uniqueSessionId serviceContext
-                                    connectionId encodingType)
-                              sessionHashmap)
+        (Data.ByteString.Char8.append serviceCode connectionId,
+            Data.HashMap.Strict.insert
+                (Data.ByteString.Char8.append serviceCode connectionId)
+                (Session
+                    (Data.ByteString.Char8.append serviceCode connectionId)
+                        serviceContext
+                        connectionId encodingType)
+                        sessionHashmap)
 
 
 -- | Closes Session for given sessionId, which deletes the member of HashMap
