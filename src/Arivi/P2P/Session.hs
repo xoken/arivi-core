8473723dd15e8d54c1b57fb77b9bf5fea0f1ce0e
Mahesh Uligade <maheshuligade@gmail.com>
2018-04-23 12:00:28 +0530

    Moved Network Session to P2P

--- src/Arivi/P2P/Session.hs
+++ src/Arivi/P2P/Session.hs
@@ -1,5 +1,5 @@
 -- |
--- Module      :  Arivi.Network.Session
+-- Module      :  Arivi.P2P.Session
 -- Copyright   :
 -- License     :
 -- Maintainer  :  Mahesh Uligade <maheshsuligade@gmail.com>
@@ -9,7 +9,7 @@
 -- This module provides useful functions for managing sessions in Arivi
 -- communication
 
-module Arivi.Network.Session
+module  Arivi.P2P.Session
 (
     SessionId,
     ServiceCode,
@@ -23,16 +23,28 @@ import           Data.ByteString.Char8    (ByteString, append, pack)
 import           Data.HashMap.Strict      (HashMap, delete, empty, insert,
                                            member)
 
+import qualified Arivi.Crypto.Utils.Keys.Encryption as Keys
 import           Arivi.Network.Connection
 import           Arivi.Network.Types      (EncodingType, ServiceCode,
-                                           ServiceContext, TransportType)
+                                           ServiceContext, TransportType,
+                                           HostAddress,PortNumber)
 
 -- | SessionId is type synonym for ByteString
 type SessionId = ByteString
+type NodeId = Keys.PublicKey
 
+  -- data SessionRequest = SessionRequest {
+  --     -- type of OPEN / CLOSE
+  --     -- UUID v4 for session identifier (both)
+  --     -- transport type (open)
+  --     -- port (open)
+  --     -- IP (open)
+  --     -- reason
+  -- }
 
-
-
+  -- data ServiceContext = ServiceContext {
+  --     TChan SessionRequest
+  -- }
 
 -- | (sessionId,Session) are (key,value) pair in HashMap that stores
 -- information about all the session uniquely
@@ -44,15 +56,21 @@ data Session = Session {
                         } deriving (Eq)
 
 
-
+-- https://github.com/xoken/arivi/blob/WireSpecification/AriviWireSpec.md
 -- | Creates Unique session and stores in given HashMap
-
 createSession :: ServiceCode
               -> ServiceContext
               -> ConnectionId
               -> EncodingType
               -> HashMap SessionId Session
               -> (SessionId,HashMap SessionId Session)
+
+-- createSession :: ServiceCode
+--               -> ServiceContext
+--               -> ConnectionId
+--               -> EncodingType
+--               -> HashMap SessionId Session
+--               -> (SessionId,HashMap SessionId Session)
 createSession serviceCode serviceContext connectionId
               encodingType sessionHashmap =
         (Data.ByteString.Char8.append serviceCode connectionId,
