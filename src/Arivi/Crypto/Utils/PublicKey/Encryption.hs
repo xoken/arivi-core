fc0dc21af7278faef1ec724aebba1d30f31dcf35
pawan <pawandhananjay@gmail.com>
2018-05-03 10:52:44 +0530

    added wrapper functions for getting shared secret

--- src/Arivi/Crypto/Utils/PublicKey/Encryption.hs
+++ src/Arivi/Crypto/Utils/PublicKey/Encryption.hs
@@ -1,6 +1,8 @@
 module Arivi.Crypto.Utils.PublicKey.Encryption
 (
     generateKeyPair,
+    getSecretKey, 
+    getPublicKey,
     toByteString, 
     createSharedSecretKey, 
     derivedSharedSecretKey
@@ -11,9 +13,7 @@ import           Crypto.ECC                             (Curve_X25519,
                                                          SharedSecret, ecdh)
 import           Crypto.Error                           (CryptoFailable,
                                                          throwCryptoError)
-import qualified Crypto.PubKey.Curve25519               (PublicKey, SecretKey,
-                                                         publicKey, secretKey,
-                                                         toPublic)
+import qualified Crypto.PubKey.Curve25519               as Curve25519
 import           Data.ByteArray                         (convert)
 import           Data.ByteString.Char8                  (ByteString)
 import           Data.Proxy
@@ -21,7 +21,7 @@ import           Data.Proxy
 import qualified Arivi.Crypto.Utils.PublicKey.Signature
 import           Arivi.Crypto.Utils.Random
 import           Crypto.Hash                            (Digest, SHA256, hash)
-import qualified Crypto.PubKey.Ed25519                  (PublicKey, SecretKey)
+import qualified Crypto.PubKey.Ed25519                  as Ed25519
 
 sha256 :: ByteString -> Digest SHA256
 sha256 = hash
@@ -30,37 +30,34 @@ sha256 = hash
 encryptionSecretDerivationFunction = sha256
 
 -- | Generate secret key for encryption using secret key of signing
-generateSecretKey :: Crypto.PubKey.Ed25519.SecretKey -> Crypto.PubKey.Curve25519.SecretKey
-
-generateSecretKey signingSecretKey = throwCryptoError secretKey
+getSecretKey :: Ed25519.SecretKey -> Curve25519.SecretKey
+getSecretKey signingSecretKey = throwCryptoError secretKey
     where
         bsSecretKey = encryptionSecretDerivationFunction (Data.ByteArray.convert signingSecretKey :: ByteString)
-        secretKey = Crypto.PubKey.Curve25519.secretKey bsSecretKey
+        secretKey = Curve25519.secretKey bsSecretKey
 
 -- | Get curve25519 public key for corresponding sk
-getPublicKey :: Crypto.PubKey.Curve25519.SecretKey -> Crypto.PubKey.Curve25519.PublicKey
-getPublicKey = Crypto.PubKey.Curve25519.toPublic
+getPublicKey :: Curve25519.SecretKey -> Curve25519.PublicKey
+getPublicKey = Curve25519.toPublic
 
 -- | Takes PublicKey as input and extracts the string part of PublicKey
-toByteString :: Crypto.PubKey.Curve25519.PublicKey -> ByteString
+toByteString :: Curve25519.PublicKey -> ByteString
 toByteString mPublicKey = Data.ByteArray.convert mPublicKey :: ByteString
 
 -- | Generate an encryption keypair
-generateKeyPair :: Crypto.PubKey.Ed25519.SecretKey -> IO(Crypto.PubKey.Curve25519.SecretKey, Crypto.PubKey.Curve25519.PublicKey)
-
+generateKeyPair :: Ed25519.SecretKey -> IO(Curve25519.SecretKey, Curve25519.PublicKey)
 generateKeyPair sk = do
-    let encryptSK = generateSecretKey sk
+    let encryptSK = getSecretKey sk
     let encryptPK = getPublicKey encryptSK
     return (encryptSK, encryptPK)
 
--- | This is Elliptic curve. user of this library don't have to worry about this
 
 curveX25519 = Proxy :: Proxy Curve_X25519
 
 -- | Using createSharedSecreatKey sender will create SharedSecret for himself
 -- and shares encrypted ephemeralPublicKey with remote
 
-createSharedSecretKey :: Crypto.PubKey.Curve25519.SecretKey -> Crypto.PubKey.Curve25519.PublicKey ->  Crypto.ECC.SharedSecret
+createSharedSecretKey :: Curve25519.SecretKey -> Curve25519.PublicKey ->  Crypto.ECC.SharedSecret
 createSharedSecretKey = ecdh curveX25519
 
 
@@ -69,5 +66,5 @@ createSharedSecretKey = ecdh curveX25519
 -- ephemeralPublicKey and computes SecretKey using derivedSharedSecreatKey
 -- function
 
-derivedSharedSecretKey :: Crypto.PubKey.Curve25519.PublicKey -> Crypto.PubKey.Curve25519.SecretKey -> Crypto.ECC.SharedSecret
+derivedSharedSecretKey :: Curve25519.PublicKey -> Curve25519.SecretKey -> Crypto.ECC.SharedSecret
 derivedSharedSecretKey ephemeralPublicKey remotePrivateKey =  ecdh curveX25519 remotePrivateKey ephemeralPublicKey
