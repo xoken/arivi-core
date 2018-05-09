d49625c2838933945914399342ab977c4de69551
Mahesh Uligade <maheshuligade@gmail.com>
2018-05-09 11:48:54 +0530

    Added makeConnectionId, concatenate methods

--- src/Arivi/Network/Connection.hs
+++ src/Arivi/Network/Connection.hs
@@ -10,26 +10,28 @@
 -- communication
 module Arivi.Network.Connection
 (
+    ConnectionId,
     Connection (..),
     getUniqueConnectionId,
     genConnectionId,
     createConnection,
-    closeConnection
+    closeConnection,
+    concatenate,
+    makeConnectionId
 ) where
 
 
 import           Arivi.Crypto.Utils.Keys.Encryption as Keys
+import qualified Crypto.PubKey.Ed25519               as Ed25519
 import           Arivi.Crypto.Utils.Random
 import           Arivi.Kademlia.Types               (HostAddress)
-import           Arivi.Network.Types                (ConnectionId, NodeId,
-                                                     Parcel (..), PeerType (..),
+import           Arivi.Network.Types                (Parcel (..), PeerType (..),
                                                      PortNumber, SequenceNum,
-                                                     TransportType)
+                                                     TransportType, NodeId, ConnectionId)
 import           Arivi.P2P.Types                    (ServiceRequest (..))
 import           Control.Concurrent.STM.TChan       (TChan)
-import qualified Crypto.PubKey.Ed25519              as Ed25519
 import           Data.ByteString.Base16             (encode)
-import           Data.ByteString.Char8              (ByteString, pack)
+import           Data.ByteString.Char8              (ByteString,append, pack)
 import           Data.HashMap.Strict                (HashMap, delete, empty,
                                                      insert, member)
 import           Network.Socket                     (Socket)
@@ -42,19 +44,20 @@ import           Network.Socket                     (Socket)
 -- | (ConnectionId,Connection) are (key,value) pair in HashMap that stores
 -- information about all the Connection uniquely
 data Connection = Connection {
-                          connectionId     :: ConnectionId
-                        , remoteNodeId     :: NodeId
-                        , ipAddress        :: HostAddress
-                        , port             :: PortNumber
-                        , ephemeralPubKey  :: NodeId
+                          connectionId    :: ConnectionId
+                        , remoteNodeId    :: NodeId
+                        , ipAddress       :: HostAddress
+                        , port            :: PortNumber
+                        , ephemeralPubKey :: NodeId
                         , ephemeralPrivKey:: Ed25519.SecretKey
-                        , transportType    :: TransportType
-                        , peerType         :: PeerType
-                        , sharedSecret     :: Keys.SharedSecret
-                        , serviceReqTChan  :: TChan ServiceRequest
-                        , parcelTChan      :: TChan Parcel
-                        , egressSeqNum     :: SequenceNum
-                        , ingressSeqNum    :: SequenceNum
+                        , transportType   :: TransportType
+                        , peerType        :: PeerType
+                        , socket          :: Socket
+                        , sharedSecret    :: Keys.SharedSecret
+                        , serviceReqTChan :: TChan ServiceRequest
+                        , parcelTChan     :: TChan Parcel
+                        , egressSeqNum    :: SequenceNum
+                        , ingressSeqNum   :: SequenceNum
                         } deriving (Eq)
 
 
@@ -79,6 +82,24 @@ getUniqueConnectionId hashmap = do
 
 
 
+-- | Takes two arguments converts them into ByteString and concatenates them
+concatenate :: (Show first, Show second) => first -> second -> ByteString
+concatenate first second = Data.ByteString.Char8.append
+                            (Data.ByteString.Char8.pack $ show first)
+                            (Data.ByteString.Char8.pack $ show second)
+
+
+-- | ConnectionId is concatenation of IP Address, PortNumber and TransportType
+makeConnectionId :: (Monad m)
+                 => HostAddress
+                 -> PortNumber
+                 -> TransportType
+                 -> m ConnectionId
+makeConnectionId ipAddress port transportType =
+                        return (concatenate
+                                  (concatenate (concatenate ipAddress "|")
+                                               (concatenate port "|"))
+                                  (concatenate transportType "|"))
 
 -- | Creates Unique Connection  and stores in given HashMap
 
@@ -89,16 +110,17 @@ createConnection :: NodeId
                  -> Ed25519.SecretKey
                  -> TransportType
                  -> PeerType
+                 -> Socket
                  -> Keys.SharedSecret
-                 -> HashMap ConnectionId Connection
                  -> TChan ServiceRequest
                  -> TChan Parcel
                  -> SequenceNum
                  -> SequenceNum
+                 -> HashMap ConnectionId Connection
                  -> IO (ConnectionId,HashMap ConnectionId Connection)
 createConnection nodeId ipAddress port ephemeralPubKey ephemeralPrivKey
-                transportType  peerType sharedSecret connectionHashmap
-                serviceRequestTChan parcelTChan egressSeqNum ingressSeqNum =
+                transportType  peerType socket sharedSecret serviceRequestTChan
+                parcelTChan egressSeqNum ingressSeqNum connectionHashmap =
 
                 getUniqueConnectionId connectionHashmap
                     >>= \uniqueConnectionId
@@ -106,9 +128,10 @@ createConnection nodeId ipAddress port ephemeralPubKey ephemeralPrivKey
                     (uniqueConnectionId,
                         Data.HashMap.Strict.insert uniqueConnectionId
                                  (Connection uniqueConnectionId nodeId
-                                                ipAddress port ephemeralPubKey ephemeralPrivKey
-                                             transportType peerType sharedSecret
-                                             serviceRequestTChan
+                                                ipAddress port ephemeralPubKey
+                                                ephemeralPrivKey
+                                             transportType peerType socket
+                                             sharedSecret serviceRequestTChan
                                              parcelTChan
                                              egressSeqNum ingressSeqNum)
                               connectionHashmap)
