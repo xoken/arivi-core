c4823aa0d0f98da3ac974ac3a95cc6c673b2d9fb
Pranay <pranay.rungta@gmail.com>
2018-05-10 11:43:11 +0530

    Stream can read Parcel

--- src/Arivi/Network/Stream.hs
+++ src/Arivi/Network/Stream.hs
@@ -28,10 +28,53 @@ runTCPServerForever sock sockAddr = do
     --  int above specifies the maximum number of queued connections and should
     --  be at least 1; the maximum value is system-dependent (usually 5).
 
-    print ("TCP Server now listening for requests at : " ++ show sockAddr)
+    print "TCP Server now listening for requests at : " -- ++ show sockAddr)
     forever $
          do
             (conn,addr) <- accept sock
             (mesg, socaddr2) <- N.recvFrom sock 4096
             close conn
             close sock
+
+
+
+
+-- Functions for Client connecting to Server
+
+
+data TransportType =  UDP|TCP deriving(Eq)
+
+getAddressType:: TransportType -> SocketType
+getAddressType  transportType = if transportType==TCP then
+                                Stream else Datagram
+
+-- Example getSocket "127.0.0.1" 3000 TCP
+getSocket :: String -> Int -> TransportType -> IO Socket
+getSocket ipAdd port transportType = withSocketsDo $ do
+    let portNo = Just (show port)
+    let transport_type = (getAddressType transportType)
+    let hints = defaultHints {addrSocketType = transport_type}
+    addr:_ <- getAddrInfo (Just hints) (Just ipAdd) portNo
+    sock <- socket AF_INET transport_type (addrProtocol addr)
+    connect sock (addrAddress addr)
+    return sock
+
+
+sendByteTo :: Socket -> C.ByteString -> IO ()
+sendByteTo sock databs = do
+    sendAll sock databs
+
+
+
+-- Converts length in byteString to Num
+getFrameLength :: Num b => S.ByteString -> b
+getFrameLength len = fromIntegral lenInt16 where
+                     lenInt16 = decode lenbs :: Int16
+                     lenbs = BSL.fromStrict len
+
+-- Reads frame a given socket
+getFrame :: Socket -> IO S.ByteString
+getFrame sock = do
+    lenbs <- recv conn 2
+    parcelCipher <- recv conn $ getFrameLength lenbs
+    return parcelCipher
