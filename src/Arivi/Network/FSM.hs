252c5640be6a74b82e9ed8ad04f595c82a2c1e83
Mahesh Uligade <maheshuligade@gmail.com>
2018-05-21 17:00:22 +0530

    Add Timeout events for Handshake,DataMessageExchange and Pinged State

--- src/Arivi/Network/FSM.hs
+++ src/Arivi/Network/FSM.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 -- |
 -- Module      :  Arivi.Network.FSM
 -- Copyright   :
@@ -16,21 +17,24 @@ module Arivi.Network.FSM (
   , initFSM
 ) where
 
+import           Arivi.Crypto.Utils.Keys.Signature
 import           Arivi.Network.Connection
 import           Arivi.Network.Types
-import           Arivi.P2P.Types          (ServiceRequest (..),
-                                           ServiceType (..))
+import           Arivi.P2P.Types                   (ServiceRequest (..),
+                                                    ServiceType (..))
 import           Control.Concurrent.Async
+import           Control.Concurrent.Killable       (kill)
 import           Control.Concurrent.STM
 import           Control.Monad.IO.Class
 import           Data.Either
-import           Arivi.Crypto.Utils.Keys.Signature
+import qualified System.Timer.Updatable            as Timer (Delay, parallel)
 
 -- | The different states that any thread in layer 1 can be in
 data State =  Idle
             | KeyExchangeInitiated
             | SecureTransportEstablished
             | Terminated
+            | Pinged
             deriving (Show, Eq)
 
 -- | The different events in layer 1 that cause state change
@@ -40,8 +44,26 @@ data Event =  InitHandshakeEvent {serviceRequest::ServiceRequest}
              | KeyExchangeInitEvent {parcel::Parcel, secretKey::SecretKey}
              | KeyExchangeRespEvent {parcel::Parcel}
              | ReceiveDataEvent {parcel::Parcel}
+             | PINGDataEvent {parcel::Parcel}
+             | PONGDataEvent {parcel::Parcel}
+             | HandshakeInitTimeOutEvent {serviceRequest::ServiceRequest}
+             | DataParcelTimeOutEvent {serviceRequest::ServiceRequest}
+             | PingTimeOutEvent {serviceRequest::ServiceRequest}
              | CleanUpEvent
-          deriving (Eq)
+             deriving (Eq)
+
+-- | Handshake Timer is  30 seconds
+handshakeTimer :: Timer.Delay
+handshakeTimer = 3*(10^7)
+
+-- | Data Message Timer 60 seconds
+dataMessageTimer :: Timer.Delay
+dataMessageTimer =  6*(10^7)
+
+-- | Ping Message Timer is 30 seconds
+pingTimer :: Timer.Delay
+pingTimer =  3*(10^7)
+
 
 -- | Initiate FSM
 initFSM :: Connection -> IO State
@@ -59,7 +81,10 @@ handleEvent :: Connection -> State -> Event -> IO State
 handleEvent connection Idle
                     (InitHandshakeEvent serviceRequest)  =
         do
+            handshakeInitTimer <- Timer.parallel (postHandshakeInitTimeOutEvent connection) handshakeTimer -- 30 seconds
+
             let nextEvent = getNextEvent connection
+            kill handshakeInitTimer
             nextEvent >>= handleEvent connection KeyExchangeInitiated
 
 --recipient will go from Idle to VersionNegotiatedState
@@ -79,17 +104,28 @@ handleEvent connection KeyExchangeInitiated
 -- Receive message from the network
 handleEvent connection SecureTransportEstablished (ReceiveDataEvent parcel) =
         do
+            dataMessageTimer <- Timer.parallel (postDataParcelTimeOutEvent connection) dataMessageTimer -- 60 seconds
             let nextEvent = getNextEvent connection
             -- TODO handleDataMessage parcel --decodeCBOR - collectFragments -
             -- addToOutputChan
-            nextEvent >>= handleEvent connection SecureTransportEstablished
+            kill dataMessageTimer -- killing died event is any exception
+            nextEvent >>= \case
+                (DataParcelTimeOutEvent _) -> (nextEvent >>= handleEvent connection Pinged)
+                _ -> (nextEvent >>= handleEvent connection SecureTransportEstablished)
+
 
 -- Receive message from p2p layer
 handleEvent connection SecureTransportEstablished (SendDataEvent serviceRequest) =
         do
             -- TODO chunk message, encodeCBOR, encrypt, send
+            dataMessageTimer <- Timer.parallel (postDataParcelTimeOutEvent connection) dataMessageTimer -- 60 seconds
             let nextEvent = getNextEvent connection
-            nextEvent >>= handleEvent connection SecureTransportEstablished
+            kill dataMessageTimer -- killing died event is any exception?
+
+            nextEvent >>= \case
+                (DataParcelTimeOutEvent _) -> (nextEvent >>= handleEvent connection Pinged)
+                _ -> (nextEvent >>= handleEvent connection SecureTransportEstablished)
+
 
 handleEvent connection SecureTransportEstablished (TerminateConnectionEvent parcel) =
         handleEvent connection Terminated CleanUpEvent
@@ -98,13 +134,31 @@ handleEvent connection Terminated CleanUpEvent =
             --- do all cleanup here
             return Terminated
 
+handleEvent connection KeyExchangeInitiated (HandshakeInitTimeOutEvent serviceRequest)=
+            return Terminated
+
+handleEvent connection Pinged (DataParcelTimeOutEvent serviceRequest) =
+        do
+            pingTimer <- Timer.parallel (postPingParcelTimeOutEvent connection) pingTimer -- 30 seconds
+            let nextEvent = getNextEvent connection
+            kill pingTimer
+            nextEvent >>= handleEvent connection Pinged
+
+
+handleEvent connection Pinged (PingTimeOutEvent serviceRequest) =
+         -- go to terminated
+            return Terminated
+
+handleEvent connection Pinged (ReceiveDataEvent parcel) =
+        do -- go to established state
+        handleEvent connection SecureTransportEstablished (ReceiveDataEvent parcel)
+
 handleEvent connection _ _  =
             handleEvent connection Terminated CleanUpEvent
 
 
 
-getNextEvent
-  :: Control.Monad.IO.Class.MonadIO m => Connection -> m Event
+getNextEvent :: Connection -> IO Event
 
 getNextEvent connection = do
             let serReqTChannel  = serviceReqTChan connection
@@ -124,6 +178,13 @@ getNextEvent connection = do
                                                             (takeLeft e))
                             SENDMSG -> return (SendDataEvent
                                                             (takeLeft e))
+                            HANDSHAKE_TIMEOUT -> return
+                                                (HandshakeInitTimeOutEvent
+                                                            (takeLeft e))
+                            DATA_TIMEOUT -> return (DataParcelTimeOutEvent
+                                                            (takeLeft e))
+                            PING_TIMEOUT -> return (PingTimeOutEvent
+                                                            (takeLeft e))
             else
                 do
                     let opcodeType = opcode (header (takeRight e))
@@ -132,6 +193,8 @@ getNextEvent connection = do
                         -- KEY_EXCHANGE_INIT -> return (KeyExchangeInitEvent secretKey (takeRight e))
                         KEY_EXCHANGE_RESP -> return (KeyExchangeRespEvent (takeRight e))
                         DATA -> return (ReceiveDataEvent (takeRight e))
+                        PING -> return (PINGDataEvent (takeRight e))
+                        PONG -> return (PONGDataEvent (takeRight e))
 
 
 
@@ -149,6 +212,25 @@ takeLeft (Left left) = left
 takeRight :: Either a b -> b
 takeRight (Right right) = right
 
+
+postHandshakeInitTimeOutEvent connection = do
+    let serviceRequestTChannel = serviceReqTChan connection
+    atomically $ writeTChan serviceRequestTChannel (TimeOutServiceRequest HANDSHAKE_TIMEOUT)
+
+postDataParcelTimeOutEvent connection = do
+    let serviceRequestTChannel = serviceReqTChan connection
+    atomically $ writeTChan serviceRequestTChannel (TimeOutServiceRequest DATA_TIMEOUT)
+
+postPingParcelTimeOutEvent connection = do
+    let serviceRequestTChannel = serviceReqTChan connection
+    atomically $ writeTChan serviceRequestTChannel (TimeOutServiceRequest PING_TIMEOUT)
+
+
+-- getEventType inputEvent = case inputEvent of
+--                          (DataParcelTimeOutEvent serviceRequest) -> DataParcelTimeOutEvent
+
+getEventType inputEvent = case inputEvent of
+                         (DataParcelTimeOutEvent serviceRequest) -> DataParcelTimeOutEvent
 
 -- | TODO replace this with actual decryption function
 -- decryptParcel parcel = KeyExParcel undefined undefined
