79122de79f57405bb2d93b5b839964dc7787efd6
pawanjay176 <pawandhananjay@gmail.com>
2018-05-28 13:59:15 +0530

    Added custom exception data types

---
+++ crypto/Arivi/Crypto/Types.hs
@@ -0,0 +1,11 @@
+module Arivi.Crypto.Types (
+    CryptoException(..)
+) where
+
+import           Control.Exception
+import           Crypto.Error      (CryptoError (..))
+
+data CryptoException =   CryptoException CryptoError
+                            | SignatureVerificationFailed deriving(Eq, Show)
+
+instance Exception CryptoException
---
+++ network/Arivi/NetworkException.hs
@@ -0,0 +1,11 @@
+module Arivi.NetworkException (
+    AriviNetworkException(..)
+) where
+
+import           Arivi.Crypto.Types (CryptoException (..))
+import           Codec.Serialise    (DeserialiseFailure)
+import           Control.Exception
+data AriviNetworkException =  AriviDeserialiseFailure DeserialiseFailure
+                            | AriviCryptoException CryptoException deriving(Show)
+
+instance Exception AriviNetworkException
