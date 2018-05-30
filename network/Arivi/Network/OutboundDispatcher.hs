91441d3934506ade26e246e403d097e0caf0213b
pawanjay176 <pawandhananjay@gmail.com>
2018-05-30 13:11:58 +0530

    Changed type of aead nonce to Int64

--- network/Arivi/Network/OutboundDispatcher.hs
+++ network/Arivi/Network/OutboundDispatcher.hs
@@ -15,22 +15,10 @@ import           Control.Concurrent.STM             (TChan, atomically,
 import qualified Data.Binary                        as Binary (decode, encode)
 import qualified Data.ByteString.Char8              as B (ByteString)
 import           Data.ByteString.Lazy               as L
-
--- | Take a bytestring, convert to Int, increment it and convert back to Bytestring
-incrementAeadNonce :: B.ByteString -> B.ByteString
-incrementAeadNonce aeadNonce = lazyToStrict incrementedNonce where
-    -- Convert to lazy bytestring
-    lazyBSNonce = strictToLazy aeadNonce
-    -- convert to integer
-    nonceAsInt = Binary.decode lazyBSNonce :: Integer
-    -- increment by 1
-    -- convert back to bytestring
-    incrementedNonce = Binary.encode (nonceAsInt + 1)
-
-
+import           Data.Int                           (Int64)
 -- | This thread is created one per connection.
 -- | It is spawned by the FSM just before going into SecureTransportEstablised state with an initial value of the aead nonce. The aead nonce is incremented accordingly each time by the recursive call
-outboundFrameDispatcher :: TChan OutboundFragment -> Connection -> B.ByteString -> Integer -> IO ()
+outboundFrameDispatcher :: TChan OutboundFragment -> Connection -> Int64 -> Integer -> IO ()
 outboundFrameDispatcher outboundTChan conn aeadnonce replayNonce = do
     (msgId, fragmentNum, fragmentCount, fragment) <- atomically $ readTChan outboundTChan
     -- Generate header
@@ -44,5 +32,5 @@ outboundFrameDispatcher outboundTChan conn aeadnonce replayNonce = do
     -- Call the send function on the socket
     sendFrame(socket conn) frame
     -- Recursively call function with nonces incremented
-    outboundFrameDispatcher outboundTChan conn (incrementAeadNonce aeadnonce) (replayNonce + 1)
+    outboundFrameDispatcher outboundTChan conn (aeadnonce+1) (replayNonce + 1)
     return ()
