6c8f6a4f7b526df35d60571c0d4a46bdf75097a7
Mahesh Uligade <maheshuligade@gmail.com>
2018-05-30 16:33:13 +0530

    Change secretKey to cryptoEnvSercretKey

--- network/Arivi/AppM.hs
+++ network/Arivi/AppM.hs
@@ -9,20 +9,22 @@ import           Arivi.Crypto.Utils.Keys.Signature
 import           Arivi.Crypto.Utils.PublicKey.Utils
 import           Arivi.Env
 import           Arivi.Logging
+import           Arivi.Network.Connection           (Connection (..),
+                                                     ConnectionId)
+import           Arivi.Network.Datagram             (createUDPSocket)
 import           Arivi.Network.Instance
 import           Arivi.Network.StreamServer
-import           Control.Concurrent.STM.TQueue
+import           Arivi.Network.Types                (Event (..), Payload (..))
+import qualified Arivi.Network.Types                as ANT
 import           Control.Concurrent.Async
+import           Control.Concurrent.STM             (atomically, newTChan,
+                                                     readTChan, writeTChan)
+import           Control.Concurrent.STM.TQueue
+import           Control.Monad                      (forever)
 import           Control.Monad.Logger
 import           Control.Monad.Reader
+import           Data.HashTable.IO                  as MutableHashMap (new)
 import           Network.Socket
-----
-import qualified Arivi.Network.Types               as ANT
-----
-import           Arivi.Network.Connection          (Connection (..),
-                                                    ConnectionId)
-import           Arivi.Network.Datagram            (createUDPSocket)
-import           Data.HashTable.IO                 as MutableHashMap (new)
 
 type AppM = ReaderT AriviEnv (LoggingT IO)
 
@@ -33,7 +35,7 @@ instance HasAriviNetworkInstance AppM where
   getAriviNetworkInstance = ariviNetworkInstance <$> getEnv
 
 instance HasSecretKey AppM where
-  getSecretKey = secretKey . ariviCryptoEnv <$> getEnv
+  getSecretKey = cryptoEnvSercretKey . ariviCryptoEnv <$> getEnv
 
 instance HasLogging AppM where
   getLoggerChan = loggerChan <$> getEnv
@@ -57,13 +59,20 @@ main = do
   let env = mkAriviEnv {  ariviCryptoEnv = CryptoEnv sk
                         , loggerChan = tq
                         , udpSocket = sock
-                        , udpConnectionHashMap = mutableConnectionHashMap}
+                        , udpConnectionHashMap = mutableConnectionHashMap
+                      }
   runStdoutLoggingT $ runAppM env (do
                                        runTCPserver (show (envPort env))
-                                       ha <- liftIO $ inet_addr "127.0.0.1"
+                                       let ha = "127.0.0.1"
                                        liftIO $ putStrLn "a"
-                                       (cid) <- openConnection ha 8080 ANT.TCP (generateNodeId sk) ANT.INITIATOR
+                                       cid <- openConnection ha 8080 ANT.TCP (generateNodeId sk) ANT.INITIATOR
 
                                        liftIO $ print ha
                                        liftIO $ print cid
+
+                                           -- runTCPserver (show (envPort env))
+                                       let ha = "127.0.0.1"
+                                       cid <- openConnection ha 8080 ANT.TCP "1" ANT.INITIATOR
+                                       liftIO $ print cid
+
                                    )
