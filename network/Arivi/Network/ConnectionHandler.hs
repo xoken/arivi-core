3ac1f22d89ea537b85658659c9fa2802b24084b3
pawanjay176 <pawandhananjay@gmail.com>
2018-06-05 14:15:30 +0530

    Added timer logic in getParcel and moved functions between modules

--- network/Arivi/Network/ConnectionHandler.hs
+++ network/Arivi/Network/ConnectionHandler.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -fno-warn-missing-fields #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
 -- |
--- Module      :  Arivi.Network.FrameDispatcher
+-- Module      :  Arivi.Network.ConnectionHandler
 -- Copyright   :
 -- License     :
 -- Maintainer  :  Mahesh Uligade <maheshuligade@gmail.com>
@@ -9,23 +11,47 @@
 --
 -- This module provides useful functions for managing dispatch of frames in
 -- Arivi communication
-module Arivi.Network.FrameDispatcher
+module Arivi.Network.ConnectionHandler
 (
-     getIPAddress
+     acceptIncomingSocket
+   , getIPAddress
    , getPortNumber
    , getTransportType
    , handleInboundConnection
+   , readHandshakeRespSock
 ) where
 
 import           Arivi.Env
 import           Arivi.Logging
 import           Arivi.Network.Connection        as Conn
 import qualified Arivi.Network.FSM               as FSM (initFSM)
+import           Arivi.Network.Handshake
+import           Arivi.Network.StreamClient
 import           Arivi.Network.Types
-import           Control.Concurrent.Async.Lifted (async)
+import           Debug.Trace
+-- import           Arivi.Network.Types             (DeserialiseFailure,
+--                                                   Event (..), Header (..),
+--                                                   Parcel (..),
+--                                                   deserialiseOrFail)
+import           Arivi.Utils.Exception
+import           Control.Concurrent              (threadDelay)
+import qualified Control.Concurrent.Async        as Async (async, race)
+import           Control.Concurrent.Async.Lifted
 import           Control.Concurrent.STM          (TChan, atomically, newTChan)
+import           Control.Concurrent.STM.TVar
+import           Control.Exception.Base
+import           Control.Monad                   (forever, void)
 import           Control.Monad.IO.Class
+import           Crypto.PubKey.Ed25519           (SecretKey)
+import           Data.Binary
+import qualified Data.ByteString                 as BS
+import qualified Data.ByteString.Lazy            as BSL
+import qualified Data.ByteString.Lazy.Char8      as BSLC
+import           Data.HashMap.Strict             as HM
+import           Data.Int
 import           Network.Socket
+import qualified Network.Socket.ByteString       as N (recv)
+
 
 -- | Reads encryptedPayload and socket from inboundTChan, constructs
 -- connectionId using `makeConnectionId`. If this connectionId is already
@@ -37,31 +63,19 @@ import           Network.Socket
 -- inboundConnectionHandler :: TChan (Socket)
 --      -> HashMap ConnectionId (TChan ByteString)
 --      -> IO (HashMap ConnectionId (TChan ByteString))
-handleInboundConnection :: (HasSecretKey m, HasLogging m) => Socket -> TChan Event -> m ()
-handleInboundConnection mSocket mEventTChan = do
-        -- mSocket <- atomically $ readTChan inboundTChan
-
-        conn <- liftIO $ do
+handleInboundConnection :: (HasAriviNetworkInstance m, HasSecretKey m, HasLogging m) => Socket -> m ()
+handleInboundConnection mSocket = do
+        sk <- getSecretKey
+        ariviInstance <- getAriviNetworkInstance
+        let tv = ariviNetworkConnectionMap ariviInstance
+        let updatesTChan = ariviConnectionUpdatesTChan ariviInstance
+        liftIO $ do
           socketName <- getSocketName mSocket
           mIpAddress <- inet_ntoa $ getIPAddress socketName
           let mPort = getPortNumber socketName
           let mTransportType = getTransportType mSocket
           let mConnectionId = Conn.makeConnectionId mIpAddress mPort mTransportType
 
-        -- if Data.HashMap.Strict.member connectionId frameDispatchHashMap
-        --     then
-        --       do
-        --         let eventTChan = fromJust (Data.HashMap.Strict.lookup
-        --                                                 connectionId
-        --                                                 frameDispatchHashMap)
-        --         atomically $ writeTChan eventTChan encryptedPayload
-        --         inboundConnectionHandler inboundTChan frameDispatchHashMap
-        -- else
-        --     do
-        -- eventTChan <- atomically newTChan
-        -- atomically $ writeTChan eventTChan encryptedPayload
-        -- serviceReqTChan <- atomically newTChan
-          outboundTChan <- atomically newTChan
           mReassemblyTChan <- atomically newTChan
           p2pMsgTChan <- atomically newTChan
           let connection = Conn.Connection { Conn.connectionId = mConnectionId
@@ -70,27 +84,18 @@ handleInboundConnection mSocket mEventTChan = do
                                            , Conn.transportType = mTransportType
                                            , Conn.personalityType = RECIPIENT
                                            , Conn.socket = mSocket
-                                           , Conn.eventTChan = mEventTChan
-                                           , Conn.outboundFragmentTChan = outboundTChan
                                            , Conn.reassemblyTChan = mReassemblyTChan
                                            , Conn.p2pMessageTChan = p2pMsgTChan
                                            }
-          return connection
-
-        -- let updatedFrameDispatchHashMap = Data.HashMap.Strict.insert
-        --                                   connectionId
-        --                                   eventTChan
-        --                                   frameDispatchHashMap
-
-        -- fsmHandle <- async (FSM.handleEvent connection FSM.Idle
-        --                 (FSM.KeyExchangeInitEvent initParcel pvtKey))
-        -- fsmHandle <- async (FSM.initFSM conn)
-        _ <- async (FSM.initFSM conn)
-        -- fsmHandle <- async (undefined)
-        -- async (readSock sock eventTChan)
-        --putStrLn ("listening on thread " ++  (show threadNo) )
-        return ()
-        -- inboundConnectionHandler inboundTChan connectionTChan-- updatedFrameDispatchHashMap
+          -- getParcel, recipientHandshake and sendFrame might fail
+          -- In any case, the thread just quits
+          parcel <- readHandshakeInitSock mSocket
+          traceShow parcel (return ())
+          (serialisedParcel, updatedConn) <- recipientHandshake sk connection parcel
+          sendFrame (Conn.socket updatedConn) (createFrame serialisedParcel)
+          atomically $ modifyTVar tv (HM.insert mConnectionId updatedConn)
+          Async.async (readSock mSocket)
+          return ()
 
 -- | Given `SockAddr` retrieves `HostAddress`
 getIPAddress :: SockAddr -> HostAddress
@@ -106,3 +111,95 @@ getPortNumber _                           = error "getPortNumber: SockAddr is no
 getTransportType :: Socket -> TransportType
 getTransportType (MkSocket _ _ Stream _ _) = TCP
 getTransportType _                         = UDP
+
+-- | Server Thread that spawns new thread to
+-- | listen to client and put it to inboundTChan
+acceptIncomingSocket :: (HasAriviNetworkInstance m, HasSecretKey m, HasLogging m) => Socket -> m a
+acceptIncomingSocket sock = do
+  sk <- getSecretKey
+  forever $ do
+        (mSocket, peer) <- liftIO $ accept sock
+        liftIO $ putStrLn $ "Connection from " ++ show peer
+        eventTChan <- liftIO $ atomically newTChan
+        -- threadDelay 5000000
+        -- sendFrame mSocket (createFrame "msg")
+        async (handleInboundConnection mSocket) --or use forkIO
+
+
+
+-- | Converts length in byteString to Num
+getFrameLength :: Num b => BS.ByteString -> b
+getFrameLength len = fromIntegral lenInt16 where
+                     lenInt16 = decode lenbs :: Int16
+                     lenbs = BSL.fromStrict len
+
+-- | Reads frame a given socket
+getParcelWithTimeout :: Socket -> Int -> IO (Either AriviException Parcel)
+getParcelWithTimeout sock timeout = do
+    winner <- race (threadDelay timeout) (N.recv sock 2)
+    case winner of
+      Left _ -> return $ Left AriviTimeoutException
+      Right lenbs ->
+        do
+          traceShow lenbs (return ())
+          parcelCipher <- N.recv sock $ getFrameLength lenbs
+          traceShow parcelCipher (return ())
+          either
+            (return . Left . AriviDeserialiseException) (return . Right)
+            (deserialiseOrFail (BSL.fromStrict parcelCipher))
+
+-- | Reads frame a given socket
+getParcel :: Socket -> IO (Either AriviException Parcel)
+getParcel sock = do
+    lenbs <- N.recv sock 2
+    parcelCipher <- N.recv sock $ getFrameLength lenbs
+    either (return . Left . AriviDeserialiseException) (return . Right)
+      (deserialiseOrFail (BSL.fromStrict parcelCipher))
+
+readSock :: Socket -> IO ()
+readSock sock =
+  traceShow "here " (return ())>>
+  getParcelWithTimeout sock 30000000 >>=
+    \case
+    Left e -> traceShow e (return ()) >> readSock sock
+    Right r -> traceShow r (return ()) >> readSock sock
+-- readSock sock eventTChan sk = forever $
+--         getParcel sock >>=
+--         either (sendFrame sock . BSLC.pack . displayException)
+--                (\case
+--                    e@(Parcel (HandshakeInitHeader _ _) _) -> do
+--                      traceShow e (return ())
+--                      atomically $ writeTChan eventTChan (KeyExchangeInitEvent e sk)
+--                    e@(Parcel (HandshakeRespHeader _ _) _) -> do
+--                     traceShow e (return ())
+--                     atomically $ writeTChan eventTChan (KeyExchangeRespEvent e)
+--                    e@(Parcel DataHeader {} _)    -> do
+--                      traceShow e (return ())
+--                      atomically $ writeTChan eventTChan (ReceiveDataEvent e)
+--                    e                                      -> do
+--                      traceShow e (return ())
+--                      sendFrame sock "O traveller! Don't wander into uncharted terriories!"
+--                )
+
+
+readHandshakeInitSock :: Socket -> IO Parcel
+readHandshakeInitSock sock = do
+  parcel <- getParcel sock
+  either throwIO return parcel
+
+
+
+-- | Read on the socket for a handshakeRespParcel and return it or throw appropriate AriviException
+readHandshakeRespSock :: Socket -> SecretKey -> IO Parcel
+readHandshakeRespSock sock sk = do
+  parcelOrFail <- getParcelWithTimeout sock 30000000
+  case parcelOrFail of
+    Left (AriviDeserialiseException e) ->
+      do
+        sendFrame sock $ BSLC.pack (displayException e)
+        throw $ AriviDeserialiseException e
+    Left e -> throw e
+    Right hsRespParcel ->
+      case hsRespParcel of
+        parcel@(Parcel (HandshakeRespHeader _ _ ) _ ) -> return parcel
+        _ -> throw AriviWrongParcelException
