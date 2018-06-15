e2234b1d06383559a1c3bf27b99fbe28979f48d9
akumar101 <avinash10207@gmail.com>
2018-06-15 17:05:42 +0530

    Add options and supported message functionality along with updation of the hashmap, arivi ticket #19

--- p2p/Arivi/P2P/P2PEnv.hs
+++ p2p/Arivi/P2P/P2PEnv.hs
@@ -1,67 +1,75 @@
-{-# LANGUAGE FlexibleContexts,     MultiParamTypeClasses #-}
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances     #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
 
 module Arivi.P2P.P2PEnv
-(
-    module Arivi.P2P.P2PEnv
-)
-where
+    ( module Arivi.P2P.P2PEnv
+    ) where
 
-import              Arivi.P2P.MessageHandler.HandlerTypes
-import              Arivi.P2P.Types
+import           Arivi.P2P.MessageHandler.HandlerTypes
+import           Arivi.P2P.RPC.Types
+import           Arivi.P2P.Types
 
-import              Data.HashMap.Strict             as HM
-import              Control.Concurrent.STM          (TVar,newTVarIO)
-import              Control.Concurrent.STM.TQueue
-import              Control.Monad.IO.Class          (MonadIO)
-import              Control.Monad.Trans.Control     (MonadBaseControl)
-import              Control.Monad.Reader            (ReaderT, ask, runReaderT)
+import           Control.Concurrent.STM                (TVar, newTVarIO)
+import           Control.Concurrent.STM.TQueue
+import           Control.Monad.IO.Class                (MonadIO)
+import           Control.Monad.Reader                  (ReaderT, ask,
+                                                        runReaderT)
+import           Control.Monad.Trans.Control           (MonadBaseControl)
+import           Data.HashMap.Strict                   as HM
 
-data P2PEnv = P2PEnv {
-    tvarAriviP2PInstance    :: TVar AriviP2PInstance,
-    tvarPeerUUIDMap         :: TVar PeerUUIDMap,
-    tqueueKadem             :: TQueue MessageInfo,
-    tqueueRPC               :: TQueue MessageInfo,
-    tqueuePubSub            :: TQueue MessageInfo,
-    tvarConnectionInfoMap   :: TVar ConnectionInfoMap
-}
+data P2PEnv = P2PEnv
+    { tvarAriviP2PInstance  :: TVar AriviP2PInstance
+    , tvarPeerUUIDMap       :: TVar PeerUUIDMap
+    , tqueueKadem           :: TQueue MessageInfo
+    , tqueueRPC             :: TQueue MessageInfo
+    , tqueuePubSub          :: TQueue MessageInfo
+    , tvarConnectionInfoMap :: TVar ConnectionInfoMap
+    , tvarResourceToPeerMap :: TVar ResourceToPeerMap
+    }
 
 type P2Papp = ReaderT P2PEnv IO
 
-class (MonadIO m, MonadBaseControl IO m) => HasP2PEnv m where
-    getP2PEnv                           :: m P2PEnv
-    getAriviTVarP2PEnv                  :: m (TVar AriviP2PInstance)
-    getpeerUUIDMapTVarP2PEnv            :: m (TVar PeerUUIDMap)
-    getkademTQueueP2PEnv                :: m (TQueue MessageInfo)
-    getrpcTQueueP2PEnv                  :: m (TQueue MessageInfo)
-    getpubsubTQueueP2PEnv               :: m (TQueue MessageInfo)
-    getConnectionInfoMapTVarP2PEnv      :: m (TVar ConnectionInfoMap)
+class (MonadIO m, MonadBaseControl IO m) =>
+      HasP2PEnv m
+    where
+    getP2PEnv :: m P2PEnv
+    getAriviTVarP2PEnv :: m (TVar AriviP2PInstance)
+    getpeerUUIDMapTVarP2PEnv :: m (TVar PeerUUIDMap)
+    getkademTQueueP2PEnv :: m (TQueue MessageInfo)
+    getrpcTQueueP2PEnv :: m (TQueue MessageInfo)
+    getpubsubTQueueP2PEnv :: m (TQueue MessageInfo)
+    getConnectionInfoMapTVarP2PEnv :: m (TVar ConnectionInfoMap)
+    getResourceToPeerMapP2PEnv :: m (TVar ResourceToPeerMap)
 
 instance HasP2PEnv P2Papp where
-    getP2PEnv                       = ask
-    getAriviTVarP2PEnv              = tvarAriviP2PInstance <$> getP2PEnv
-    getpeerUUIDMapTVarP2PEnv        = tvarPeerUUIDMap <$> getP2PEnv
-    getkademTQueueP2PEnv            = tqueueKadem <$> getP2PEnv
-    getrpcTQueueP2PEnv              = tqueueRPC <$> getP2PEnv
-    getpubsubTQueueP2PEnv           = tqueuePubSub <$> getP2PEnv
-    getConnectionInfoMapTVarP2PEnv  = tvarConnectionInfoMap <$> getP2PEnv
+    getP2PEnv = ask
+    getAriviTVarP2PEnv = tvarAriviP2PInstance <$> getP2PEnv
+    getpeerUUIDMapTVarP2PEnv = tvarPeerUUIDMap <$> getP2PEnv
+    getkademTQueueP2PEnv = tqueueKadem <$> getP2PEnv
+    getrpcTQueueP2PEnv = tqueueRPC <$> getP2PEnv
+    getpubsubTQueueP2PEnv = tqueuePubSub <$> getP2PEnv
+    getConnectionInfoMapTVarP2PEnv = tvarConnectionInfoMap <$> getP2PEnv
+    getResourceToPeerMapP2PEnv = tvarResourceToPeerMap <$> getP2PEnv
 
 runP2Papp :: P2PEnv -> P2Papp a -> IO a
 runP2Papp = flip runReaderT
 
 makeP2PEnvironment :: IO P2PEnv
 makeP2PEnvironment = do
-
     pmap <- newTVarIO HM.empty
     kqueue <- newTQueueIO
     rqueue <- newTQueueIO
     pqueue <- newTQueueIO
     cmap <- newTVarIO HM.empty
-
-    return P2PEnv {
-        tvarPeerUUIDMap = pmap,
-        tqueueKadem = kqueue,
-        tqueueRPC = rqueue,
-        tqueuePubSub = pqueue,
-        tvarConnectionInfoMap = cmap
-                }
+    r2pmap <- newTVarIO HM.empty
+    return
+        P2PEnv
+            { tvarPeerUUIDMap = pmap
+            , tqueueKadem = kqueue
+            , tqueueRPC = rqueue
+            , tqueuePubSub = pqueue
+            , tvarConnectionInfoMap = cmap
+            , tvarResourceToPeerMap = r2pmap
+            }
--- p2p/Arivi/P2P/RPC/Functions.hs
+++ p2p/Arivi/P2P/RPC/Functions.hs
@@ -1,17 +1,18 @@
-module Arivi.P2P.RPC.Functions
-    ( module Arivi.P2P.RPC.Functions
-    ) where
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import           Arivi.Network.Types              (ConnectionId, NodeId,
-                                                   TransportType (..))
+module Arivi.P2P.RPC.Functions where
+
+import           Arivi.Network.Types                   (ConnectionId, NodeId,
+                                                        TransportType (..))
 import           Arivi.P2P.MessageHandler.Handler
+import           Arivi.P2P.MessageHandler.HandlerTypes (Peer (..))
 import           Arivi.P2P.RPC.Types
-import           Control.Concurrent               (forkIO, threadDelay)
+import           Control.Concurrent                    (forkIO, threadDelay)
 import           Control.Concurrent.STM.TQueue
 import           Control.Concurrent.STM.TVar
-import           Control.Monad                    (forever)
+import           Control.Monad                         (forever)
 import           Control.Monad.STM
-import           Data.HashMap.Strict              as HM
+import           Data.HashMap.Strict                   as HM
 import           Data.Maybe
 
 {-
@@ -23,26 +24,26 @@ import           Data.Maybe
 --if not good then return getResource(resourceID serviceMessage)
 -- else return (serviceMessage ret)
 -}
-registerResource ::
-       ServiceId -> ResourceList -> TVar ResourceToPeerMap -> IO () -- Maybe here is used to return nothing for now
-registerResource _ [] _ = return ()
+{-
+  structure of HashMap entry => key : ResourceId, value : (ServiceId, TQueue Peers)
+-}
+-- registers all the resources requested by a service in a HashMap
+registerResource :: ServiceId -> ResourceList -> TVar ResourceToPeerMap -> IO ()
+registerResource _ [] _ = return () -- recursion corner case
 registerResource serviceId (resource:resourceList) resourceToPeerMap = do
     peers <- newTQueueIO -- create a new empty Tqueue
-    atomically
+    atomically -- read Tvar , update HashMap, write it back
         (do resourceToPeerMapTvar <- readTVar resourceToPeerMap --
             let temp =
-                    HM.insert resource (serviceId, peers) resourceToPeerMapTvar
+                    HM.insert resource (serviceId, peers) resourceToPeerMapTvar --
             writeTVar resourceToPeerMap temp)
     registerResource serviceId (resource : resourceList) resourceToPeerMap
 
--- worker thread
--- should read the hashMap
--- should check if the number of peers for a resource is less than some number
--- if it is less should ask kademlia for more nodes
+-- creates a worker thread
+-- thread should read the hashMap and should check if the number of peers for a resource is less than some number
+-- if it is less should ask Kademlia for more nodes
 -- send each peer and option message
--- do this for each peer
--- then for each entry in the hash map
---registerResource serviceCode resourceList
+-- the options message module will handle the sending of messages and updating of the HashMap based on the support message
 checkPeerInResourceMap :: TVar ResourceToPeerMap -> IO ()
 checkPeerInResourceMap resourceToPeerMapTvar = do
     let minimumPeers = 5
@@ -56,15 +57,15 @@ checkPeerInResourceMapHelper resourceToPeerMapTvar minimumPeers =
         let tempList = HM.toList resourceToPeerMap
         listOfLengths <- extractListOfLengths tempList
         let numberOfPeers = minimumPeers - minimum listOfLengths
-                                                                                                                        -- ask peers from kademlia
         if numberOfPeers > 0
-                                                                                                                                        --askKademliaForPeers
-                                                                                                                                        -- send options message
+            --askKademliaForPeers
+            -- send options message
             then threadDelay (40 * 1000)
             else threadDelay (30 * 1000) -- in milliseconds
         return ()
 
--- function to find the Tqueue with minimum length
+-- function to find the TQueue with minimum length
+-- used by the worker thread
 extractListOfLengths :: [(ResourceId, (ServiceId, TQueue Peer))] -> IO [Int]
 extractListOfLengths [] = return [0]
 extractListOfLengths (x:xs) = do
@@ -77,49 +78,19 @@ extractListOfLengths (x:xs) = do
     lenNextTQ <- extractListOfLengths xs
     return $ len : lenNextTQ
 
+-- write the Peers flushed from the TQueue back to the TQueue
 writeBackToTQueue :: TQueue Peer -> [Peer] -> STM ()
 writeBackToTQueue _ [] = return ()
 writeBackToTQueue currTQ (currentElem:listOfTQ) = do
     writeTQueue currTQ currentElem
     writeBackToTQueue currTQ listOfTQ
 
--- should eventually write into int option message module
--- basically option message module will handle the sending options and putting peers into HashMap
-updateResourcePeers :: (Peer, [ResourceId]) -> TVar ResourceToPeerMap -> IO Int
-updateResourcePeers peerResourceTuple resourceToPeerMapTvar = do
-    resourceToPeerMap <- readTVarIO resourceToPeerMapTvar
-    let peer = fst peerResourceTuple
-    let listOfResources = snd peerResourceTuple
-    updateResourcePeersHelper peer listOfResources resourceToPeerMap
-
--- write a wrapper for this
--- adds the peer to the TQueue of each resource
--- lookup for the current resource in the HashMap
--- assumes that the resourceIDs are present in the HashMap
--- cannot add new currently because the serviceID is not available
-updateResourcePeersHelper ::
-       Peer -> [ResourceId] -> ResourceToPeerMap -> IO Int
-updateResourcePeersHelper _ [] _ = return 0
-updateResourcePeersHelper peer (currResource:listOfResources) resourceToPeerMap = do
-    let temp = HM.lookup currResource resourceToPeerMap
-                                                                                                                                                                -- check for lookup returning Nothing
-    if isNothing temp
-        then updateResourcePeersHelper peer listOfResources resourceToPeerMap
-        else do
-            let currTQ = snd (fromJust temp)
-            atomically (writeTQueue currTQ peer)
-            tmp <-
-                updateResourcePeersHelper
-                    peer
-                    listOfResources
-                    resourceToPeerMap
-            return $ 1 + tmp
-
 -- DUMMY FUNCTION !!!
+-- signature of the function to ask Kademlia for peers
 askKademliaForPeers :: Int -> Peer -> [Peer]
 askKademliaForPeers numberOfPeers peer = [peer]
 {-
 acceptRequest (ResourceID)
---read from ResourceID Tchan
+--read from ResourceID TChan
 return (serviceMessage,
 -}
