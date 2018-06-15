10cff618f0dc40a63ff6b84ff6e126b6eb198e81
Mahesh Uligade <maheshuligade@gmail.com>
2018-06-15 12:07:33 +0530

    Replace hsBufferTChan with inboundDatagramTChan

--- network/Arivi/Network/Connection.hs
+++ network/Arivi/Network/Connection.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+
 -- |
 -- Module      :  Arivi.Network.Connection
 -- Copyright   :
@@ -9,15 +10,13 @@
 --
 -- This module provides useful functions for managing connections in Arivi
 -- communication
-
 module Arivi.Network.Connection
-(
-    ConnectionId
-  , Connection (..)
-  , HandshakeStatus(..)
-  , concatenate
-  , genConnectionId
-  , makeConnectionId
+( ConnectionId
+, Connection(..)
+, HandshakeStatus(..)
+, concatenate
+, genConnectionId
+, makeConnectionId
 ) where
 
 import           Arivi.Crypto.Utils.Keys.Encryption as Keys
@@ -39,55 +38,49 @@ import qualified Network.Socket                     as Network (HostName,
                                                                 SockAddr,
                                                                 Socket)
 
-data Connection = Connection {
-                          connectionId      :: ConnectionId
-                        , remoteNodeId      :: NodeId
-                        , ipAddress         :: Network.HostName
-                        , port              :: PortNumber
-                        , ephemeralPubKey   :: Curve25519.PublicKey
-                        , ephemeralPrivKey  :: Ed25519.SecretKey
-                        , transportType     :: TransportType
-                        , personalityType   :: PersonalityType
-                        , socket            :: Network.Socket
-                        , sharedSecret      :: Keys.SharedSecret
-                        , remoteSockAddr    :: Network.SockAddr
-                        , reassemblyTChan   :: TChan Parcel
-                        , hsBufferTChan     :: TChan Parcel
-                        , p2pMessageTChan   :: TChan L.ByteString
-                        , egressSeqNum      :: TVar SequenceNum
-                        , ingressSeqNum     :: TVar SequenceNum-- need not be TVar
-                        , aeadNonceCounter  :: TVar AeadNonce
-                        , handshakeComplete :: TVar HandshakeStatus
-                        } deriving (Eq, Generic)
+data Connection = Connection
+    { connectionId         :: ConnectionId
+    , remoteNodeId         :: NodeId
+    , ipAddress            :: Network.HostName
+    , port                 :: PortNumber
+    , ephemeralPubKey      :: Curve25519.PublicKey
+    , ephemeralPrivKey     :: Ed25519.SecretKey
+    , transportType        :: TransportType
+    , personalityType      :: PersonalityType
+    , socket               :: Network.Socket
+    , sharedSecret         :: Keys.SharedSecret
+    , remoteSockAddr       :: Network.SockAddr
+    , reassemblyTChan      :: TChan Parcel
+    , inboundDatagramTChan :: TChan Parcel
+    , p2pMessageTChan      :: TChan L.ByteString
+    , egressSeqNum         :: TVar SequenceNum
+    , ingressSeqNum        :: TVar SequenceNum -- need not be TVar
+    , aeadNonceCounter     :: TVar AeadNonce
+    , handshakeComplete    :: TVar HandshakeStatus
+    } deriving (Eq, Generic)
 
-data HandshakeStatus = HandshakeNotStarted
-                     | HandshakeInitiated
-                     | HandshakeDone
-                     deriving (Eq,Generic,Show)
+data HandshakeStatus
+    = HandshakeNotStarted
+    | HandshakeInitiated
+    | HandshakeDone
+    deriving (Eq, Generic, Show)
+
 -- | Generates a random 4 Byte ConnectionId using Raaz's random ByteString
 -- generation
 genConnectionId :: IO ByteString
-genConnectionId = getRandomByteString 4 >>=
-                                    \byteString -> return (encode byteString)
-
+genConnectionId =
+    getRandomByteString 4 >>= \byteString -> return (encode byteString)
 
 -- | Takes two arguments converts them into ByteString and concatenates them
 concatenate :: (Show first, Show second) => first -> second -> ByteString
-concatenate first second = Data.ByteString.Char8.append
-                           (Data.ByteString.Char8.pack $ show first)
-                           (Data.ByteString.Char8.pack $ show second)
-
+concatenate first second =
+    Data.ByteString.Char8.append
+        (Data.ByteString.Char8.pack $ show first)
+        (Data.ByteString.Char8.pack $ show second)
 
 -- | ConnectionId is concatenation of IP Address, PortNumber and TransportType
-makeConnectionId :: Network.HostName
-                 -> PortNumber
-                 -> TransportType
-                 -> ConnectionId
+makeConnectionId ::
+       Network.HostName -> PortNumber -> TransportType -> ConnectionId
 makeConnectionId mIpAddress mPort mTransportType =
-
-                          Data.ByteString.Char8.pack $   mIpAddress
-                                                     ++ "|"
-                                                     ++ show mPort
-                                                     ++ "|"
-                                                     ++ show mTransportType
-
+    Data.ByteString.Char8.pack $
+    mIpAddress ++ "|" ++ show mPort ++ "|" ++ show mTransportType
