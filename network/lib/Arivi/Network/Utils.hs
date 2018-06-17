d756816993f6764100693e7216d71c08cdb08152
Mahesh Uligade <maheshuligade@gmail.com>
2018-06-17 18:30:31 +0530

    Add ipvs support for getIPAddress and getPortNumber functions

--- network/lib/Arivi/Network/Utils.hs
+++ network/lib/Arivi/Network/Utils.hs
@@ -9,38 +9,37 @@
 -- This module provides useful utility functions for the Arivi Network Layer
 --
 module Arivi.Network.Utils
-(
-    lazyToStrict
-  , strictToLazy
-  , getIPAddress
-  , getPortNumber
-) where
-
+    ( lazyToStrict
+    , strictToLazy
+    , getIPAddress
+    , getPortNumber
+    ) where
 
 import           Data.ByteString      (ByteString)
 import qualified Data.ByteString.Lazy as Lazy (ByteString, fromStrict, toStrict)
-import           Network.Socket       (HostAddress, PortNumber, SockAddr (..))
-
+import           Network.Socket       (HostName, PortNumber, SockAddr (..),
+                                       inet_ntoa)
 
 -- | Converts lazy ByteString to Strict ByteString
 -- TODO: Find usage and depreacate this function
 lazyToStrict :: Lazy.ByteString -> ByteString
-lazyToStrict =  Lazy.toStrict
-
+lazyToStrict = Lazy.toStrict
 
 -- | Converts strict ByteString to lazy ByteString
 -- TODO: Find usage and depreacate this function
 strictToLazy :: ByteString -> Lazy.ByteString
 strictToLazy = Lazy.fromStrict
 
-
-
 -- | Given `SockAddr` retrieves `HostAddress`
-getIPAddress :: SockAddr -> HostAddress
-getIPAddress (SockAddrInet _ hostAddress) = hostAddress
-getIPAddress _                            = error "getIPAddress: SockAddr is not of constructor SockAddrInet "
+getIPAddress :: SockAddr -> IO HostName
+getIPAddress (SockAddrInet _ hostAddress) = inet_ntoa hostAddress
+getIPAddress (SockAddrInet6 _ _ (_, _, _, hA6) _) = inet_ntoa hA6
+getIPAddress _ =
+    error "getIPAddress: SockAddr is not of constructor SockAddrInet "
 
 -- | Given `SockAddr` retrieves `PortNumber`
 getPortNumber :: SockAddr -> PortNumber
 getPortNumber (SockAddrInet portNumber _) = portNumber
-getPortNumber _                           = error "getPortNumber: SockAddr is not of constructor SockAddrInet "
+getPortNumber (SockAddrInet6 portNumber _ _ _) = portNumber
+getPortNumber _ =
+    error "getPortNumber: SockAddr is not of constructor SockAddrInet "
