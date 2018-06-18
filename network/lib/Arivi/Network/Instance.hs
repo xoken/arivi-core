e118d2374fc9b2fb125e9b325dd257211b0df861
Mahesh Uligade <maheshuligade@gmail.com>
2018-06-18 19:14:42 +0530

    Fix network lint errors

--- network/lib/Arivi/Network/Instance.hs
+++ network/lib/Arivi/Network/Instance.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE Rank2Types            #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE Rank2Types          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Arivi.Network.Instance
     ( openConnection
@@ -7,20 +7,20 @@ module Arivi.Network.Instance
 
 import           Arivi.Env
 import           Arivi.Logging
-import           Arivi.Network.Connection             as Conn
+import           Arivi.Network.Connection        as Conn
 import           Arivi.Network.ConnectionHandler
 import           Arivi.Network.Handshake
 import           Arivi.Network.StreamClient
-import           Arivi.Network.Types                  as ANT ( ConnectionHandle (..)
-                                                             , NodeId
-                                                             , PersonalityType(..)
-                                                             , TransportType (..))
+import           Arivi.Network.Types             as ANT (ConnectionHandle (..),
+                                                         NodeId,
+                                                         PersonalityType (..),
+                                                         TransportType (..))
 import           Arivi.Utils.Exception
-import           Control.Exception                    (try)
+import           Control.Exception               (try)
 
 import           Control.Monad.Reader
-import           Crypto.PubKey.Ed25519                (SecretKey)
-import           Data.HashMap.Strict                  as HM
+import           Crypto.PubKey.Ed25519           (SecretKey)
+import           Data.HashMap.Strict             as HM
 import           Data.IORef
 import           Network.Socket
 
