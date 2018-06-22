72975f96eb4b3ffdca4e18c1910cc85e50c0607d
Ankit Singh <ankitsiam@gmail.com>
2018-06-22 15:05:13 +0530

    Implements statsd wrapper to send or push metrices to statsd server

--- utils/Arivi/Utils/Statsd.hs
+++ utils/Arivi/Utils/Statsd.hs
@@ -3,22 +3,44 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module Arivi.Utils.Statsd
-    ( StatsdClient(..)
+    ( StatsdClient
     , HasStatsdClient(..)
     , incrementCounter
     , decrementCounter
+    , createStatsdClient
+    , createStatsdClientWithHmac
+    , StatsdM
+    , counter
+    , time
     ) where
 
-import           Control.Concurrent
-import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import           Control.Monad.Trans.Control
+import           Data.Time.Units
 import qualified Network.Statsd              as S
 import           Network.Statsd.UdpClient
 
 -- | Structure to hold the stasd client
 type StatsdClient = UdpClient
+
+-- | Creates a statsd client which can be used to send unencypted messages
+--   to a statsd server
+createStatsdClient :: String -> Int -> String -> IO StatsdClient
+createStatsdClient hostname port prefix = do
+    let str = "statsd://" ++ hostname ++ ":" ++ show port ++ "/" ++ prefix
+    S.statsdClient str
+
+-- | Creates a statsd client which can be used to send encrypted messages
+--   to a statsd server using packet signing with shared secret HMAC SHA256
+--   signatures
+createStatsdClientWithHmac ::
+       String -> Int -> String -> String -> IO StatsdClient
+createStatsdClientWithHmac hostname port secret prefix = do
+    let str =
+            "statsd://:" ++
+            secret ++ "@" ++ hostname ++ ":" ++ show port ++ "/" ++ prefix
+    S.statsdClient str
 
 -- | Abstract class which defines a function to get a statsd client
 class (MonadIO m, MonadBaseControl IO m) =>
@@ -26,19 +48,31 @@ class (MonadIO m, MonadBaseControl IO m) =>
     where
     getStatsdClient :: m StatsdClient
 
--- | icrements and sends a counter given it's label
+-- | Initiates a counter with a given value and sends the metric to statsd server
+counter :: (HasStatsdClient m) => S.Stat -> Int -> m ()
+counter label init = do
+    client <- getStatsdClient
+    liftIO $ S.count client label init
+
+-- | Icrements and sends a counter given it's label
 incrementCounter :: (HasStatsdClient m) => S.Stat -> m ()
 incrementCounter label = do
     client <- getStatsdClient
     liftIO $ S.increment client label
 
--- decrement and sends a counter given it's label
+-- Decrement and sends a counter given it's label
 decrementCounter :: (HasStatsdClient m) => S.Stat -> m ()
 decrementCounter label = do
     client <- getStatsdClient
     liftIO $ S.decrement client label
 
-type AppM = ReaderT StatsdClient IO
+-- | Sends a timing metric to statsd server
+time :: (HasStatsdClient m) => S.Stat -> Millisecond -> m ()
+time label tms = do
+    client <- getStatsdClient
+    liftIO $ S.timing client label tms
 
-instance HasStatsdClient AppM where
+type StatsdM = ReaderT StatsdClient IO
+
+instance HasStatsdClient StatsdM where
     getStatsdClient = ask
