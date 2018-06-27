58f8645d0f57d45f9ab2e2c5eed3d31ec7fee870
akumar101 <avinash10207@gmail.com>
2018-06-27 15:24:07 +0530

    Maintainance function for Watchers

--- p2p/Arivi/P2P/PubSub/Functions.hs
+++ p2p/Arivi/P2P/PubSub/Functions.hs
@@ -1,22 +1,81 @@
-module Arivi.P2P.PubSub.Types
+module Arivi.P2P.PubSub.Functions
     (
     ) where
 
+import           Arivi.P2P.P2PEnv
 import           Arivi.P2P.PubSub.Types
+import           Control.Concurrent.Lifted   (fork)
+import           Control.Concurrent.STM.TVar
+import           Control.Monad.IO.Class      (liftIO)
+import           Control.Monad.STM
+import qualified Data.HashMap.Strict         as HM
+import           Data.Maybe
+import           Data.SortedList
+import           Data.Time.Clock
 
 -- | Called by each service to register its Topics. Creates entries in TopicMap
 -- the TopicHandler passed takes a topicmessage and returns a topic
 -- if topicmessage is returned it is sent to all watchers
 -- it can also return exception if the block is valid or invalidaccordingly peers are judged
-registerTopic :: (HasP2PEnv m) => [(Topic, TopicHandler, Int)] -> m ()
+-- #registerTopic :: (HasP2PEnv m) => [(Topic, TopicHandler, Int)] -> m ()
 -- | called by a service when some content created by it needs to be broadcasted to the network ie notifiers as well as subscribers
-publishTopic :: (HasP2PEnv m) => Topic -> TopicMessage -> m ()
+-- #publishTopic :: (HasP2PEnv m) => Topic -> TopicMessage -> m ()
 -- | spawns a thread with the parameter TVar TopicMap and checks expired subscribers and deletes entry
 maintainWatchers :: (HasP2PEnv m) => m ()
 -- | spawns a thread with the parameter TVar TopicMap and checks expired notifiers and possible resubscribe to the peer based on rep. also checks if min number if satisfied and add more peers accordingly by calling send options which takes peer from kad
-maintainNotifiers :: (HasP2PEnv m) => m ()-- -- | called by a service to read the TopicMessage TQueue
+-- #maintainNotifiers :: (HasP2PEnv m) => m ()-- -- | called by a service to read the TopicMessage TQueue
 -- readTopic :: (HasP2PEnv m) => TopicId -> m TopicMessage
 -- -- | called by a service after it has verified a previously read TopicMessage to broadcast it further to the subscribers. only sent to Subscribers minus nodes in MessageHashMap
 -- notifyTopic :: (HasP2PEnv m) => TopicId -> TopicMessage -> m ()\
 -- -- | called in case of a unvalid block and used for peer rep based on MessageHashMap
 -- flagMessage :: (HasP2PEnv m) => TopicId -> TopicMessage -> m ()
+{-
+maintain watchers checks the watchers list
+assuming that the watchers are sorted
+need to spawn a thread that will traverse the watchers list for each topic
+get the list of keys for the hashmap and use that to traverse
+for each key get the list of watchers and start traversing
+drop all the watchers that have expired and since it is sorted if a watcher is not expired kill the thread
+and continue for the other keys
+-}
+maintainWatchers = do
+    watcherTableTVar <- getWatcherTableP2PEnv
+    watcherMap <- liftIO $ readTVarIO watcherTableTVar
+    let topicIds = HM.keys watcherMap
+    fork (maintainWatchersHelper watcherMap topicIds)
+    return ()
+
+maintainWatchersHelper :: (HasP2PEnv m) => WatchersTable -> [Topic] -> m ()
+maintainWatchersHelper _ [] = return ()
+maintainWatchersHelper watcherMap (topic:topicIdList) = do
+    let currListTvarMaybe = HM.lookup topic watcherMap
+    let currListTvar = fromJust currListTvarMaybe
+    currTime <- liftIO getCurrentTime
+    liftIO $
+        atomically
+            (do currSortedList <- readTVar currListTvar
+                let currList = fromSortedList currSortedList
+                let newList = checkWatchers currList currTime
+                let newSortedList = toSortedList newList
+                writeTVar currListTvar newSortedList)
+
+-- will take the list of watchers for each topic and check their validity
+checkWatchers :: [Watcher] -> UTCTime -> [Watcher]
+checkWatchers [] _ = []
+checkWatchers (currWatcher:watcherList) currentTime =
+    if timer currWatcher < currentTime
+        then [] ++ checkWatchers watcherList currentTime
+        else currWatcher : watcherList
+-- checkWatchers :: [Watcher] -> IO [Watcher]
+-- checkWatchers [] = return []
+-- checkWatchers (currWatcher : watcherList) =
+-- 	do
+-- 		let watcherTime = timer currWatcher
+-- 		currentTime <- getCurrentTime
+-- 		if (currentTime > watcherTime) -- then the watcher has expired
+-- 			then
+-- 				do
+-- 					nextWatcher <- checkWatchers watcherList
+-- 					return nextWatcher
+-- 			else
+-- 				return (currWatcher : watcherList)
