2e7869f67be3971fe72aadec370f6d658a4ec47d
pawanjay176 <pawandhananjay@gmail.com>
2018-07-10 17:53:07 +0530

    Creates dummy service for testing p2p rpc

---
+++ service/lib/Service/HelloWorld.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Service.HelloWorld
+where
+
+import           Arivi.P2P.P2PEnv
+import           Arivi.P2P.RPC.Functions
+import           Arivi.Utils.Logging
+import           Control.Monad.IO.Class
+import           Data.ByteString.Lazy    as Lazy
+-- import           Service.Types                    (ResourceType (Archived))
+import           Arivi.P2P.RPC.Types     (ResourceType (..))
+
+type ServiceMsg = Lazy.ByteString
+
+handler :: Lazy.ByteString -> Lazy.ByteString
+handler serviceMsg = Lazy.concat [serviceMsg, "Praise Jesus"]
+
+getResourceId :: String
+getResourceId = "HelloWorld"
+
+registerHelloWorld :: (HasP2PEnv m, HasLogging m) => m ()
+registerHelloWorld = registerResource getResourceId handler Archived
+
+getHelloWorld :: (HasP2PEnv m, HasLogging m) => m ()
+getHelloWorld = do
+    resource <- getResource getResourceId "HelloWorld"
+    liftIO $ print "here"
+    liftIO $ print resource
