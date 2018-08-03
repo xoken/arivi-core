7caed52de6b09458120d1db1d2ce93bd6685b667
Mahesh Uligade <maheshuligade@gmail.com>
2018-08-03 17:37:12 +0530

    Add cronjob script to run arivi if it is created in last hour Close #143

--- scripts/Deployment-Tools/cronejob.sh
+++ scripts/Deployment-Tools/cronejob.sh
@@ -1,4 +1,10 @@
-source /home/jenkins/ssh-agent.cf
-if [ `find arivi-network-test -mmin -180` ]; then
-    python fabfile.py Main
+# source /home/jenkins/ssh-agent.cf
+if [ `find /home/xkn/Main -mmin -60` ]; then
+    # if pgrep '/home/xkn/Main .' 2>/dev/null; then
+    #     # kill -9 $(ps aux | grep '/home/xkn/Main .' | awk '{print $2}') 2 > /dev/null
+    #     echo "Terminating /home/xkn/Main" >> /home/xkn/Main.log
+    #     pkill -f "/home/xkn/Main ."
+    # fi
+    pkill -f "/home/xkn/Main ."
+    nohup bash `/home/xkn/Main .` > nohup.out 2> nohup.err < /dev/null &
 fi
