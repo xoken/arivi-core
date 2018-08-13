f50eb09d52f3e30aef73a2fa32368211432ce4f8
Mahesh Uligade <maheshuligade@gmail.com>
2018-08-13 18:05:02 +0530

    Update Jenkinsfile and fabfiles

---
+++ scripts/Deployment-Tools/gitcommit.sh
@@ -0,0 +1,202 @@
+
+  git checkout master
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout Crypto
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout Peer-Reputation
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout PubSub-Enhancement
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout UDP
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout UDP2
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout WireSpecification
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout avinash-RPCErrors
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout crypto
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout crypto-tests
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout development
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout dummy-service
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout experimental
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout feature/kademlia
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout jenkins
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout kademlia
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout kaushal-statsd
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout master
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout network-peer
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout node-endpoint
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout p2pdev
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout p2pdevelopment
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout remove
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout test-automation
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+  git checkout use-packcheck
+  cp /home/mahesh/Videos/arivi/temp/fabfile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/logdumpFabFile.py /home/mahesh/Videos/arivi/scripts/Deployment-Tools/
+  cp /home/mahesh/Videos/arivi/temp/Jenkinsfile /home/mahesh/Videos/arivi/
+  git add /home/mahesh/Videos/arivi/scripts/Deployment-Tools
+  git add /home/mahesh/Videos/arivi/Jenkinsfile
+  git commit -m "Update Jenkinsfile and fabfiles"
+
+
