df32a94509f4955fc1c8b09cb4cff45daa0451eb
Mahesh Uligade <maheshuligade@gmail.com>
2018-08-17 17:51:52 +0530

    Fix build warnings

--- p2p/lib/Arivi/P2P/PRT/Exceptions.hs
+++ p2p/lib/Arivi/P2P/PRT/Exceptions.hs
@@ -2,9 +2,7 @@ module Arivi.P2P.PRT.Exceptions
     ( PRTExecption(..)
     ) where
 
-import           Codec.Serialise   (DeserialiseFailure)
 import           Control.Exception
-import           Crypto.Error      (CryptoError)
 
 data PRTExecption =
     PeerDeedNotFound
