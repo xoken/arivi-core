cca29503f84824f8d692a9d833ea6b14157fd8c8
Mahesh Uligade <maheshuligade@gmail.com>
2018-08-20 11:47:57 +0530

    Refine Peer Reputation code

--- p2p/lib/Arivi/P2P/PRT/Exceptions.hs
+++ p2p/lib/Arivi/P2P/PRT/Exceptions.hs
@@ -1,11 +1,25 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      : Arivi.P2P.PRT.Exceptions
+-- License     :
+-- Maintainer  : Mahesh Uligade <maheshuligade@gmail.com>
+-- Stability   :
+-- Portability :
+--
+-- This module defines different types of Exceptions that comes in Peer
+-- Reputation management
+--
+--------------------------------------------------------------------------------
 module Arivi.P2P.PRT.Exceptions
     ( PRTExecption(..)
     ) where
 
 import           Control.Exception
 
+-- | Types of Exceptions that comes in Peer Reputation management
 data PRTExecption =
-    PeerDeedNotFound
+    PeerDeedNotFound -- ^ Throw this Exception whene given Peer NodeId not
+                     --   found in the PeerReputation hashmap
     deriving (Show)
 
 instance Exception PRTExecption
