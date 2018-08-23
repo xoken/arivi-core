c5a46e8eb85b4d843e9d4af271c3d62deae5c733
Mahesh Uligade <maheshuligade@gmail.com>
2018-08-23 16:55:44 +0530

    Add helper functions for calculating No Of Peers in Reputed,Closest and Random category

--- p2p/lib/Arivi/P2P/PRT/Instance.hs
+++ p2p/lib/Arivi/P2P/PRT/Instance.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 --------------------------------------------------------------------------------
 -- |
 -- Module      : Arivi.P2P.PRT.Types
@@ -19,6 +21,7 @@ module Arivi.P2P.PRT.Instance
     , updatePeerReputationForP2P
     , updatePeerReputationForServices
     , getReputation
+    , getWeightages
     ) where
 
 import qualified Arivi.Network.Types         as Network (NodeId)
@@ -32,6 +35,8 @@ import           Control.Exception           (throw)
 import           Control.Monad.IO.Class      (liftIO)
 import           Control.Monad.STM           (atomically)
 import qualified Data.HashMap.Strict         as HM (insert, lookup)
+import           Data.Ratio                  (Ratio, Rational, denominator,
+                                              numerator)
 import           Data.Yaml                   (ParseException, decodeFileEither)
 
 -- | Reads the config file and converts it's fields to config data type
@@ -42,15 +47,37 @@ loadConfigFile filePath = do
         Left e       -> throw e
         Right config -> return config
 
+isValidRatio :: Integral a => Ratio a -> Bool
+isValidRatio mRatio = do
+    let mNumerator = numerator mRatio
+    let mDenominator = denominator mRatio
+    let totalRatio = mNumerator + mDenominator
+    let firstTerm = (mNumerator * 100) `div` totalRatio
+    let secondTerm = (mDenominator * 100) `div` totalRatio
+    case firstTerm + secondTerm of
+        100 -> True
+        _   -> False
+
 -- | This  function loads the fields of config file into respective HashMap
 loadPRTConfigToHashMap :: (HasP2PEnv m) => m ()
 loadPRTConfigToHashMap = do
     mConfig <- liftIO $ loadConfigFile "PRTConfig.yaml"
     servicesReputationHashMapTVar <- getServicesReputationHashMapTVar
     p2pReputationHashMapTVar <- getP2PReputationHashMapTVar
+    reputedVsOtherTVar <- getReputedVsOtherTVar
+    kClosestVsRandomTVar <- getKClosestVsRandomTVar
     liftIO $
         atomically $ writeTVar servicesReputationHashMapTVar (services mConfig)
     liftIO $ atomically $ writeTVar p2pReputationHashMapTVar (p2p mConfig)
+    if isValidRatio (reputedVsOther mConfig :: Rational)
+        then liftIO $
+             atomically $ writeTVar reputedVsOtherTVar (reputedVsOther mConfig)
+        else throw InvalidRatioReputedVsOther
+    if isValidRatio (kClosestVsRandom mConfig :: Rational)
+        then liftIO $
+             atomically $
+             writeTVar kClosestVsRandomTVar (kClosestVsRandom mConfig)
+        else throw InvalidRatioKClosestVsRandom
     return ()
 
 -- | Gives the `Reputation` of given `PeerDeed` in case of P2P
@@ -124,3 +151,37 @@ getReputation peerNodeId = do
     case HM.lookup peerNodeId mapOfAllPeersHistory of
         Just peerHistoryTable -> return $ Just $ reputation peerHistoryTable
         Nothing               -> return Nothing
+
+-- | Sums the denominator and numerator of the given Rational
+getTotal :: Rational -> Integer
+getTotal mRatio = numerator mRatio + denominator mRatio
+
+-- | Gives the no of NonReputed Peers from given k based on the given config
+getNoOfNonReputed :: Integer -> Rational -> Integer
+getNoOfNonReputed k mReputedVsOther =
+    k * denominator mReputedVsOther `div` getTotal mReputedVsOther
+
+-- | Gives the no of Closest Peers from given k based on the given config
+getnoOfClosest :: Integer -> Rational -> Integer
+getnoOfClosest nonReputedNo mKClosestVsRandom =
+    nonReputedNo * numerator mKClosestVsRandom `div` getTotal mKClosestVsRandom
+
+-- | Gives the no of Random Peers from given k based on the given config
+getnoOfRandom :: Integer -> Rational -> Integer
+getnoOfRandom nonReputedNo mKClosestVsRandom =
+    nonReputedNo * denominator mKClosestVsRandom `div`
+    getTotal mKClosestVsRandom
+
+-- | Given the total no of Peers this function splits it into Reputed,Closest
+-- and Random based on the weightages defined in the config file
+getWeightages :: (HasP2PEnv m) => Integer -> m (Integer, Integer, Integer)
+getWeightages k = do
+    reputedVsOtherTVar <- getReputedVsOtherTVar
+    mReputedVsOther <- liftIO $ readTVarIO reputedVsOtherTVar
+    kClosestVsRandomTVar <- getKClosestVsRandomTVar
+    mKClosestVsRandom <- liftIO $ readTVarIO kClosestVsRandomTVar
+    let noOfNonReputed = getNoOfNonReputed k mReputedVsOther
+    let noOfClosest = getnoOfClosest noOfNonReputed mKClosestVsRandom
+    let noOfRandom = getnoOfRandom noOfNonReputed mKClosestVsRandom
+    let noOfReputed = k - (noOfClosest + noOfRandom)
+    return (noOfReputed, noOfClosest, noOfRandom)
