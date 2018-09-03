1aad68356aec81bec9edf8288135d435d80cc438
Mahesh Uligade <maheshuligade@gmail.com>
2018-09-03 11:49:51 +0530

    Temp branch code

--- p2p/lib/Arivi/P2P/ServiceRegistry.hs
+++ p2p/lib/Arivi/P2P/ServiceRegistry.hs
@@ -1,7 +1,10 @@
 {-# OPTIONS_GHC -fprint-potential-instances #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE RankNTypes       #-}
-{-# LANGUAGE DuplicateRecordFields, KindSignatures, DataKinds, GADTs #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE RankNTypes            #-}
 
 module Arivi.P2P.ServiceRegistry
     ( makeP2Pinstance
@@ -12,13 +15,14 @@ import           Arivi.Env
 import           Arivi.P2P.Kademlia.MessageHandler
 import           Arivi.P2P.MessageHandler.HandlerTypes
 import           Arivi.P2P.P2PEnv
-import           Arivi.P2P.RPC.Functions
-import           Arivi.P2P.RPC.SendOptions
+
+-- import           Arivi.P2P.RPC.Functions
+-- import           Arivi.P2P.RPC.SendOptions
 import           Arivi.P2P.Types
 import           Arivi.Utils.Logging
 import           Arivi.Utils.Statsd
-import           Control.Concurrent.STM
 import           Codec.Serialise
+import           Control.Concurrent.STM
 import           Data.HashMap.Strict                   as HM
 
 import           Control.Monad.Reader
@@ -39,28 +43,19 @@ makeP2Pinstance ::
 makeP2Pinstance nodeid mIp tcpport udpport statsdIP statsdPort statsdPrefix sk sbound pingThreshold kademliaConcurrencyFactor = do
     newStatsdClient <- createStatsdClient statsdIP statsdPort statsdPrefix
     let netENV = mkAriviEnv (read $ show tcpport) (read $ show udpport) sk
-        nc     = NetworkConfig nodeid mIp tcpport udpport
+        nc = NetworkConfig nodeid mIp tcpport udpport
     -- TODO:  need to make port consistent
-    p2p' <-
-        makeP2PEnvironment
-            nc
-            sbound
-            pingThreshold
-            kademliaConcurrencyFactor
+    p2p' <- makeP2PEnvironment nc sbound pingThreshold kademliaConcurrencyFactor
     let p2pEnv =
             p2p'
-                { ariviNetworkEnv = netENV
-                , _networkConfig = nc
-                , statsdClient = newStatsdClient
-                , tvarMessageTypeMap = handlers
-                }
+            { ariviNetworkEnv = netENV
+            , _networkConfig = nc
+            , statsdClient = newStatsdClient
+                -- , tvarMessageTypeMap = handlers
+            }
     return p2pEnv
-
-
-
-handlers :: Handlers
-handlers = Handler rpcHandler kademliaHandler
-
+-- handlers :: Handlers
+-- handlers = Handler rpcHandler kademliaHandler
 {-
 data Handler m = forall t msg. Handler (Request t msg -> m (Response t msg))
 
