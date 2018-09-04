8e788fc9766aa10faa131057aafaaaba7ca30774
Mahesh Uligade <maheshuligade@gmail.com>
2018-09-04 17:32:53 +0530

    Fix hlint errors

--- p2p/lib/Arivi/P2P/RPC.hs
+++ p2p/lib/Arivi/P2P/RPC.hs
@@ -1,13 +1,12 @@
 module Arivi.P2P.Rpc where
 
-import Arivi.P2P.Types
-import Arivi.P2P.MessageHandler.HandlerTypes
+import           Arivi.P2P.MessageHandler.HandlerTypes
+import           Arivi.P2P.Types
 
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as Map
-import Data.ByteString
-import Control.Concurrent.STM
+import           Control.Concurrent.STM
+import           Data.ByteString
+import           Data.HashMap.Strict                   (HashMap)
+import qualified Data.HashMap.Strict                   as Map
 
-initRPC :: Map k (Handler msg IO) -> Map NodeId ([PeerDetails]) -> IO ()
-initRPC handlers peers = do
-  return ()
+initRPC :: Map k (Handler msg IO) -> Map NodeId [PeerDetails] -> IO ()
+initRPC handlers peers = return ()
