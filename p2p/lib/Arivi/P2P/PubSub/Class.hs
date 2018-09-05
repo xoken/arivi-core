9c0c0331a0a0fab1ac4285d040ae787b14c3118f
pawanjay176 <pawandhananjay@gmail.com>
2018-09-05 14:57:07 +0530

    publish to nodes in subscribers and notifiers

--- p2p/lib/Arivi/P2P/PubSub/Class.hs
+++ p2p/lib/Arivi/P2P/PubSub/Class.hs
@@ -15,9 +15,12 @@ class HasSubscribers m t where
   subscribers :: m (Subscribers t)
   -- | subscribers who have not sent us this notify
   subscribers':: (Hashable msg) => msg -> t -> m (Set NodeId)
+  subscribersForTopic :: t -> m (Set NodeId)
 
 class HasNotifiers m t where
   notifiers :: m (Notifiers t)
+  -- | Returns set of nodeIds for the given topic
+  notifiersForTopic :: t -> m (Set NodeId)
 
 class HasTopicHandlers m t msg where
   topicHandlers :: m (TopicHandlerMap t msg)
--- p2p/lib/Arivi/P2P/PubSub/Publish.hs
+++ p2p/lib/Arivi/P2P/PubSub/Publish.hs
@@ -18,10 +18,14 @@ import Control.Lens
 import Control.Monad.IO.Class
 import Control.Monad.Except
 import Data.Hashable
+import Data.Set (union)
+
+
 
 publish ::
        ( HasP2PEnv env m r msg
        , HasSubscribers m t
+       , HasNotifiers m t
        , Hashable t
        , Eq t
        , Serialise t
@@ -29,18 +33,16 @@ publish ::
     => Publish t msg
     -> m ()
 publish req@(Publish t _) = do
-    Subscribers subs <- subscribers
-    case subs ^. at t of
-        Nothing -> return ()
-        Just ns -> do
-            nodes <- liftIO $ readTVarIO ns
-            responses <-
-                mapSetConcurrently
-                   (\node -> runExceptT $ issueRequest node (PubSubRequest req))
-                    nodes
-            void $ traverseSet
-               (\case
-                     Left _ -> return ()
-                     Right (PubSubResponse Ok) -> return ()
-                     Right (PubSubResponse Error) -> return ())
-               responses
+    subs <- subscribersForTopic t
+    notf <- notifiersForTopic t
+    let nodes = subs `union` notf
+    responses <-
+        mapSetConcurrently
+            (\node -> runExceptT $ issueRequest node (PubSubRequest req))
+            nodes
+    void $ traverseSet
+        (\case
+                Left _ -> return ()
+                Right (PubSubResponse Ok) -> return ()
+                Right (PubSubResponse Error) -> return ())
+        responses
