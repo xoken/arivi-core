70a52f456202ad100a8306ec8f2fa1bc23c5e688
Pranay Sashank <pranaysashank@gmail.com>
2018-09-10 13:29:17 +0530

    Creates a HasPubSubEnv class.

--- p2p/lib/Arivi/P2P/PubSub/Env.hs
+++ p2p/lib/Arivi/P2P/PubSub/Env.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -fno-warn-orphans  #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE FunctionalDependencies#-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE UndecidableInstances  #-}
@@ -20,8 +21,8 @@ import Control.Applicative
 import Control.Concurrent.MVar
 import Control.Concurrent.STM
 import Control.Lens
+import Control.Monad
 import Control.Monad.IO.Class
-import Control.Monad.Reader
 import Data.Hashable
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -37,22 +38,25 @@ data PubSubEnv f t msg = PubSubEnv
     , pubSubHandlers :: TopicHandlers t msg
     }
 
+class HasPubSubEnv m t msg | m -> t msg where
+    pubSubEnv :: m (PubSubEnv f t msg)
+
 instance ( Ord t
          , Eq t
          , Hashable t
          , Eq msg
          , Hashable msg
-         , MonadReader (PubSubEnv TVar t msg) m
+         , HasPubSubEnv m t msg
          , MonadIO m
          ) =>
          HasSubscribers m t msg where
     subscribers t = do
-        Subscribers subs <- asks pubSubSubscribers
+        Subscribers subs <- pubSubSubscribers <$> pubSubEnv
         case subs ^. at t of
             Just x -> liftIO $ readTVarIO x
             Nothing -> return Set.empty
     subscribers' msg t = do
-        Inbox inbox <- join $ liftIO . readTVarIO <$> asks pubSubInbox
+        Inbox inbox <- join $ liftIO . readTVarIO <$> (pubSubInbox <$> pubSubEnv)
         case inbox ^. at msg of
             Just x -> liftA2 (Set.\\) (subscribers t) (liftIO $ readTVarIO x)
             -- |Invariant this branch is never reached.
@@ -60,10 +64,10 @@ instance ( Ord t
             -- to ask who sent it. Returning all subscribers.
             Nothing -> liftA2 (Set.\\) (subscribers t) (pure Set.empty)
     newSubscriber nid _ t = do
-        topics <- asks pubSubTopics
+        topics <- pubSubTopics <$> pubSubEnv
         if Set.member t topics
             then do
-                Subscribers subs <- asks pubSubSubscribers
+                Subscribers subs <- pubSubSubscribers <$> pubSubEnv
                 case subs ^. at t of
                     Just x -> do
                         liftIO . atomically $ modifyTVar x (Set.insert nid)
@@ -74,15 +78,15 @@ instance ( Ord t
                     Nothing -> return False
             else return False
 
-instance (Eq t, Hashable t, MonadReader (PubSubEnv TVar t msg) m, MonadIO m) =>
+instance (Eq t, Hashable t, HasPubSubEnv m t msg, MonadIO m) =>
          HasNotifiers m t where
     notifiers t = do
-        Notifiers notifs <- asks pubSubNotifiers
+        Notifiers notifs <- pubSubNotifiers <$> pubSubEnv
         case notifs ^. at t of
             Just x -> liftIO $ readTVarIO x
             Nothing -> return Set.empty
     newNotifier nid t = do
-        Notifiers notifs <- asks pubSubNotifiers
+        Notifiers notifs <- pubSubNotifiers <$> pubSubEnv
         case notifs ^. at t of
             Just x -> liftIO . atomically $ modifyTVar x (Set.insert nid)
             -- |Invariant this branch is never reached.
@@ -94,13 +98,13 @@ instance ( Eq t
          , Hashable t
          , Eq msg
          , Hashable msg
-         , MonadReader (PubSubEnv TVar t msg) m
+         , HasPubSubEnv m t msg
          , MonadIO m
          ) =>
          HasTopicHandlers m t msg where
     handleTopic nid t msg = do
         -- Add node to the inbox
-        inboxed <- asks pubSubInbox
+        inboxed <- pubSubInbox <$> pubSubEnv
         Inbox inbox <- liftIO $ readTVarIO inboxed
         case inbox ^. at msg of
             Just x -> liftIO . atomically $ modifyTVar x (Set.insert nid)
@@ -112,7 +116,7 @@ instance ( Eq t
                         (\(Inbox i) -> Inbox (i & at msg ?~ def))
         -- Return a response if already computed or add this message
         -- to cache map and wait for the result
-        cached <- asks pubSubCache
+        cached <- pubSubCache <$> pubSubEnv
         Cache cache <- liftIO $ readTVarIO cached
         case cache ^. at msg of
             Just x -> liftIO $ readMVar x
@@ -122,7 +126,7 @@ instance ( Eq t
                     modifyTVar
                         cached
                         (\(Cache c) -> Cache (c & at msg ?~ def))
-                TopicHandlers handlers <- asks pubSubHandlers
+                TopicHandlers handlers <- pubSubHandlers <$> pubSubEnv
                 case handlers ^. at t of
                     Just (TopicHandler h) -> do
                         resp <- h msg
